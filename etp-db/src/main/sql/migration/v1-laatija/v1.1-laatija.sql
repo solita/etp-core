830c6a38b3e8656527b601243ccc3d7191a99612
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-01-29 15:06:45 +0200

    first tables

---
+++ etp-db/src/main/sql/migration/v1-laatija/v1.1-laatija.sql
@@ -0,0 +1,7 @@
+create table laatija (
+  id serial,
+  kayttajatunnus varchar(32),
+  hetu varchar(50),
+  etunimi varchar(50),
+  sukunimi varchar(50)
+);
---
+++ etp-db/src/main/sql/migration/v2-energiatodistus/v2.1-energiatodistus.sql
@@ -0,0 +1,3 @@
+create table energiatodistus (
+  id serial
+);
