e1714ea705d7afc33e7088cb29d70bf24b8ec81b
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-02-06 12:17:16 +0200

    AE-45 move comments

--- etp-backend/src/test/clj/solita/etp/example_test.clj
+++ etp-backend/src/test/clj/solita/etp/example_test.clj
@@ -3,16 +3,17 @@
             [clojure.java.jdbc :as jdbc]
             [solita.etp.test-system :as test-system]))
 
+;; These tests should be removed when we have real tests
+;; For now these are used to verify that tests running
+;; in parallel have separate systems (separate databases)
+
+
+
 (defn create-laatija! [db laatija]
   (jdbc/insert! db :etp.laatija laatija))
 
 (defn get-laatija-count [db]
   (-> db (jdbc/query ["SELECT COUNT(id) FROM etp.laatija"]) first :count))
-
-;; These tests should be removed when we have real tests
-;; For now these are used to verify that tests running
-;; in parallel have separate systems (separate databases)
-
 
 (t/deftest example-1
   (let [{:keys [db] :as systems} (test-system/start!)]
