b001ffbb984374bd7c903a57d3b1bb73ecca58fa
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-02-10 14:59:40 +0200

    AE-44 - format

--- etp-backend/src/main/clj/solita/etp/api/yritys.clj
+++ etp-backend/src/main/clj/solita/etp/api/yritys.clj
@@ -10,18 +10,18 @@
   [["/yritykset"
     [""
      {:post {:summary    "Tallenna yrityksen tiedot"
-            :parameters {:body yritys-schema/YritysSave}
-            :responses  {201 {:body common-schema/Id}}
-            :handler    (fn [{:keys [db body-params uri]}]
-                          (api-response/created uri
-                            (yritys-service/add-yritys! db body-params)))}}]
+             :parameters {:body yritys-schema/YritysSave}
+             :responses  {201 {:body common-schema/Id}}
+             :handler    (fn [{:keys [db body-params uri]}]
+                           (api-response/created uri
+                             (yritys-service/add-yritys! db body-params)))}}]
 
     ["/:id"
-      {:get {:summary    "Hae yrityksen tiedot"
-             :parameters {:path {:id schema/Num}}
-             :responses  {200 {:body yritys-schema/Yritys}
-                          404 {:body schema/Str}}
-             :handler    (fn [{{{:keys [id]} :path} :parameters :keys [db]}]
-                           (api-response/get-response
-                             (yritys-service/find-yritys db id)
-                             (str "Yritys " id " does not exists.")))}}]]])
+     {:get {:summary    "Hae yrityksen tiedot"
+            :parameters {:path {:id schema/Num}}
+            :responses  {200 {:body yritys-schema/Yritys}
+                         404 {:body schema/Str}}
+            :handler    (fn [{{{:keys [id]} :path} :parameters :keys [db]}]
+                          (api-response/get-response
+                            (yritys-service/find-yritys db id)
+                            (str "Yritys " id " does not exists.")))}}]]])
