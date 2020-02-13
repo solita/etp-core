e087300d037276b8d7542ecc0be77e52efea0809
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-02-13 16:53:39 +0200

    AE-22 implement schema for laatija

---
+++ etp-backend/src/main/clj/solita/etp/schema/laatija.clj
@@ -0,0 +1,22 @@
+(ns solita.etp.schema.laatija
+  (:require [schema.core :as schema]
+            [solita.etp.schema.common :as common-schema]))
+
+;; TODO missing things real typing for patevyys, fields for voimassaoloaika,
+;; laskentaohjelmistot and toiminta-alueet. Needs specs...
+
+(def LaatijaSave
+  "This schema is used in add-laatija and update-laatija services"
+  (merge {:etunimi          schema/Str
+          :sukunimi         schema/Str
+          :henkilotunnus    common-schema/Hetu
+          :email            schema/Str
+          :puhelin          schema/Str
+          :patevyys         schema/Str
+          :julkinen-puhelin schema/Bool
+          :julkinen-email   schema/Bool}
+         common-schema/Postiosoite))
+
+(def Laatija
+  "Laatija schema contains basic information about persistent laatija"
+  (assoc LaatijaSave :id schema/Num))
