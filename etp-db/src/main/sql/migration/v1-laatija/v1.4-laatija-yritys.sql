2d31349f8f2a28f07e32b881314312950cdfe7d5
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-02-17 12:57:28 +0200

    AE-22 add primary key to laatija_yritys table

--- etp-db/src/main/sql/migration/v1-laatija/v1.4-laatija-yritys.sql
+++ etp-db/src/main/sql/migration/v1-laatija/v1.4-laatija-yritys.sql
@@ -1,4 +1,5 @@
 create table laatija_yritys (
   laatija_id int references laatija (id),
-  yritys_id int references yritys (id)
+  yritys_id int references yritys (id),
+  primary key (laatija_id, yritys_id)
 );
