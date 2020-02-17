067f643a040746f46a08e1744ef5015683bad9fc
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-02-17 15:05:12 +0200

    AE-62 add new fields to laatija schema

--- etp-backend/src/main/clj/solita/etp/schema/laatija.clj
+++ etp-backend/src/main/clj/solita/etp/schema/laatija.clj
@@ -6,19 +6,21 @@
   {:id schema/Int
    :label schema/Str})
 
-;; TODO missing things real typing for patevyys, fields for voimassaoloaika,
-;; laskentaohjelmistot and toiminta-alueet. Needs specs...
+;; TODO missing fields for voimassaoloaika, laskentaohjelmistot and
+;; toiminta-alueet.
 
 (def LaatijaSave
   "This schema is used in add-laatija and update-laatija services"
-  (merge {:etunimi          schema/Str
-          :sukunimi         schema/Str
-          :henkilotunnus    common-schema/Hetu
-          :email            schema/Str
-          :puhelin          schema/Str
-          :patevyys         schema/Int
-          :julkinen-puhelin schema/Bool
-          :julkinen-email   schema/Bool}
+  (merge {:etunimi           schema/Str
+          :sukunimi          schema/Str
+          :henkilotunnus     common-schema/Hetu
+          :email             schema/Str
+          :puhelin           schema/Str
+          :patevyys          schema/Int
+          :patevyys-voimassa schema/Bool
+          :julkinen-puhelin  schema/Bool
+          :julkinen-email    schema/Bool
+          :ensitallennus     schema/Bool}
          common-schema/Postiosoite))
 
 (def Laatija
