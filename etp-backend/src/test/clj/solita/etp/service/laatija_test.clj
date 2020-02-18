0af04eca10280fd87de17f00f28edda73948a32b
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-02-18 13:27:31 +0200

    AE-62 add test for pätevyydet

--- etp-backend/src/test/clj/solita/etp/service/laatija_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/laatija_test.clj
@@ -14,3 +14,7 @@
   (doseq [laatija (repeatedly 100 #(g/generate schema/LaatijaSave hetu-generator))
           :let [id (service/add-laatija! ts/*db* laatija)]]
     (t/is (= (assoc laatija :id id) (service/find-laatija ts/*db* id)))))
+
+(t/deftest find-patevyydet-test
+  (t/is (= ["Perustaso" "Ylempi taso"] (->> (service/find-patevyydet)
+                                             (map :label)))))
