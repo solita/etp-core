1cc671ae2d2e7320c7b679e6f7001fc4642774c3
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-02-21 11:20:47 +0200

    AE-67 - db exception translation for unique-violation (23505). Exception handler for unique exception.

--- etp-backend/src/main/clj/solita/etp/db.clj
+++ etp-backend/src/main/clj/solita/etp/db.clj
@@ -1,7 +1,9 @@
 (ns solita.etp.db
   (:require [integrant.core :as ig]
             [hikari-cp.core :as hikari]
-            [jeesql.core :as jeesql]))
+            [jeesql.core :as jeesql]
+            [clojure.string :as str])
+  (:import (org.postgresql.util PSQLException)))
 
 (defmethod ig/init-key :solita.etp/db
   [_ opts]
@@ -11,10 +13,35 @@
   [_ {:keys [datasource]}]
   (hikari/close-datasource datasource))
 
+(defn with-db-exception-translation [db-function args]
+  (try
+    (apply db-function args)
+    (catch PSQLException psqle
+      (let [error (.getServerErrorMessage psqle)]
+        (throw
+          (case (.getSQLState error)
+            "23505"
+            (ex-info
+              (.getMessage psqle)
+              {:type       :unique-violation
+               :constraint (keyword (str/replace (.getConstraint error) "_" "-"))}
+              psqle)
+            psqle))))))
+
+(def original-generate-query-fn jeesql.generate/generate-query-fn)
+
+(defn- generate-query-fn [ns query query-options]
+  (let [db-function (original-generate-query-fn ns query query-options)]
+    (with-meta
+      (fn [& args]
+        (with-db-exception-translation db-function args))
+      (meta db-function))))
+
 (defn require-queries
   ([name] (require-queries name {}))
   ([name options]
    (let [db-namespace (symbol (str "solita.etp.db." name))]
      (binding [*ns* (create-ns db-namespace)]
-       (jeesql/defqueries (str "solita/etp/db/" name ".sql") options))
+       (with-redefs [jeesql.generate/generate-query-fn generate-query-fn]
+         (jeesql/defqueries (str "solita/etp/db/" name ".sql") options)))
      (alias (symbol (str name "-db")) db-namespace))))
