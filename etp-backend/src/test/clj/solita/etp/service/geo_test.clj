803cd70d459c8940cf769f4d3a16137c4f43b529
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-02-28 12:25:53 +0200

    ae-83 - refactoring: :label-se -> :label-sv, because swedish language code is sv.

--- etp-backend/src/test/clj/solita/etp/service/geo_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/geo_test.clj
@@ -8,7 +8,7 @@
 (t/deftest find-toimintaalueet-test
   (let [toimintaalueet (service/find-toimintaalueet)
         fi-labels (set (map :label-fi toimintaalueet))
-        se-labels (set (map :label-se toimintaalueet))]
+        se-labels (set (map :label-sv toimintaalueet))]
     (t/is (every? #(contains? fi-labels %) ["Pirkanmaa" "Etelä-Savo"]))
     (t/is (every? #(contains? se-labels %) ["Birkaland" "Södra Savolax"]))
     (t/is (-> fi-labels (contains? "Ahvenanmaa") not))
