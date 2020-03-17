787b3259b4df16496be0085c79784f6aa94cae1e
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-03-17 12:45:37 +0200

    AE-111 - exception logging

--- etp-backend/src/main/clj/solita/etp/exception.clj
+++ etp-backend/src/main/clj/solita/etp/exception.clj
@@ -1,14 +1,26 @@
 (ns solita.etp.exception
   (:require [reitit.ring.middleware.exception :as exception]
-            [ring.util.response :as r]))
+            [clojure.tools.logging :as log]))
 
-(defn unique-exception-handler [exception _]
+(defn unique-exception-handler [exception request]
   (let [error (ex-data exception)]
+    (log/info (str "Unique violation: " (name (:constraint error))
+                   " in service: " (:uri request)))
     {:status  409
      :body    error}))
+
+(defn default-handler
+  "Default safe handler for any exception."
+  [^Throwable e request]
+  (do
+    (log/error e (str "Exception in service: " (:uri request)))
+    {:status 500
+     :body {:type "exception"
+            :class (.getName (.getClass e))}}))
 
 (def exception-middleware
   (exception/create-exception-middleware
     (assoc exception/default-handlers
+      :default default-handler
       :unique-violation unique-exception-handler)))
 
