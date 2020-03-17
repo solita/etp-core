73d6364a84e19128fd76f2ec11a19618b3960c9c
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-17 16:44:54 +0200

    AE-86 separate api layer of kayttaja and laatija

--- etp-backend/src/main/clj/solita/etp/api/kayttaja.clj
+++ etp-backend/src/main/clj/solita/etp/api/kayttaja.clj
@@ -1,10 +1,24 @@
 (ns solita.etp.api.kayttaja
-  (:require [schema.core :as schema]
+  (:require [ring.util.response :as r]
+            [schema.core :as schema]
+            [solita.etp.api.response :as api-response]
+            [solita.etp.schema.common :as common-schema]
             [solita.etp.schema.kayttaja :as kayttaja-schema]
-            [ring.util.response :as r]))
+            [solita.etp.schema.laatija :as laatija-schema]
+            [solita.etp.service.laatija :as laatija-service]))
 
 (def routes
-  [["/kayttajat/current"
+  [["/whoami"
     {:get {:summary "Kirjautuneen käyttäjän tiedot"
            :responses {200 {:body kayttaja-schema/Kayttaja}}
-           :handler (constantly (r/response {:id 1234 :username "Testi"}))}}]])
+           :handler (constantly (r/response {:id 1234 :username "Testi"}))}}]
+   ["/kayttajat"
+    ["/:id"
+     {:get {:summary "Hae käyttäjän tiedot (laatijarekisterin tiedot mukaanlukien)"
+            :parameters {:path {:id common-schema/Key}}
+            :responses {200 {:body laatija-schema/Laatija}
+                        404 {:body schema/Str}}
+            :handler (fn [{{{:keys [id]} :path} :parameters :keys [db]}]
+                       (-> (laatija-service/find-laatija db id)
+                           (api-response/get-response
+                            (str "Käyttäjä " id " does not exist."))))}}]]])
