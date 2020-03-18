88eaa78b0f6b2452dcb61d5185a96685244aea3c
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-18 16:16:40 +0200

    AE-86 add permissions to kayttaja table for etp_app

--- etp-db/src/main/sql/migration/afterMigrate.sql
+++ etp-db/src/main/sql/migration/afterMigrate.sql
@@ -2,6 +2,7 @@
 grant select on all tables in schema etp to etp_app;
 grant usage on schema etp to etp_app;
 
+grant insert, update on table kayttaja to etp_app;
 grant insert, update on table laatija to etp_app;
 grant insert, update on table energiatodistus to etp_app;
 grant insert, update on table yritys to etp_app;
