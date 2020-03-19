2ee3b746796a120bb6010cb1db80624d874d9053
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-03-19 09:48:57 +0200

    AE-13 - 404 error handling for: PUT /laatijat/:id/yritykset/:yritys-id

--- etp-backend/src/main/clj/solita/etp/api/response.clj
+++ etp-backend/src/main/clj/solita/etp/api/response.clj
@@ -1,5 +1,6 @@
 (ns solita.etp.api.response
-  (:require [ring.util.response :as r]))
+  (:require [ring.util.response :as r])
+  (:import (clojure.lang ExceptionInfo)))
 
 (defn get-response [body not-found]
   (if (nil? body)
@@ -10,6 +11,24 @@
   (if (= updated 0)
     (r/not-found not-found)
     (r/response nil)))
+
+(defn- matches-description? [error error-description]
+  (let [matcher (dissoc error-description :response)
+        matched-error (select-keys error (keys matcher))]
+    (= matcher matched-error)))
+
+(defn response-with-exceptions [service-fn error-descriptions]
+  (try
+    (do
+      (r/response (service-fn)))
+    (catch ExceptionInfo e
+      (let [error (ex-data e)
+            description (first (filter (partial matches-description? error)
+                                       error-descriptions))]
+        (if (nil? description) (throw e)
+          {:status  (:response description)
+           :headers {}
+           :body    error})))))
 
 (defn created [path id]
   (r/created (str path "/" id) {:id id}))
