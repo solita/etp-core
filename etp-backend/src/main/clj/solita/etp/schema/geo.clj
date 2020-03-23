8da1c09f73d63a5704ac73472a9d5ec18f5a0441
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-23 11:30:04 +0200

    AE-86 split Postiosoite schema

--- etp-backend/src/main/clj/solita/etp/schema/geo.clj
+++ etp-backend/src/main/clj/solita/etp/schema/geo.clj
@@ -4,11 +4,13 @@
 
 (defn valid-maa? [maa] (= (count maa) 2))
 
-(def Postiosoite
-  {:jakeluosoite     schema/Str
-   :postinumero      schema/Str
-   :postitoimipaikka schema/Str
-   :maa              (schema/constrained schema/Str valid-maa?)})
+(def PostiosoiteWithoutMaa {:jakeluosoite     schema/Str
+                            :postinumero      schema/Str
+                            :postitoimipaikka schema/Str})
+
+(def Postiosoite (assoc PostiosoiteWithoutMaa
+                        :maa
+                        (schema/constrained schema/Str valid-maa?)))
 
 (def Toimintaalue common-schema/Luokittelu)
 
