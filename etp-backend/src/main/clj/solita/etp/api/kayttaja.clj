d362e21ecca4a02af3fa0842bcf90ee261344f25
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-24 13:58:12 +0200

    AE-9 add GET endpoint for laatija by kayttaja id

--- etp-backend/src/main/clj/solita/etp/api/kayttaja.clj
+++ etp-backend/src/main/clj/solita/etp/api/kayttaja.clj
@@ -4,7 +4,9 @@
             [solita.etp.api.response :as api-response]
             [solita.etp.schema.common :as common-schema]
             [solita.etp.schema.kayttaja :as kayttaja-schema]
-            [solita.etp.service.kayttaja :as kayttaja-service]))
+            [solita.etp.schema.laatija :as laatija-schema]
+            [solita.etp.service.kayttaja :as kayttaja-service]
+            [solita.etp.service.laatija :as laatija-service]))
 
 (def routes
   [["/whoami"
@@ -13,11 +15,21 @@
            :handler (constantly (r/response {:id 1234 :username "Testi"}))}}]
    ["/kayttajat"
     ["/:id"
-     {:get {:summary "Hae käyttäjän tiedot"
-            :parameters {:path {:id common-schema/Key}}
-            :responses {200 {:body kayttaja-schema/Kayttaja}
-                        404 {:body schema/Str}}
-            :handler (fn [{{{:keys [id]} :path} :parameters :keys [db]}]
-                       (-> (kayttaja-service/find-kayttaja db id)
-                           (api-response/get-response
-                            (str "Käyttäjä " id " does not exist."))))}}]]])
+     [""
+      {:get {:summary "Hae käyttäjän tiedot"
+             :parameters {:path {:id common-schema/Key}}
+             :responses {200 {:body kayttaja-schema/Kayttaja}
+                         404 {:body schema/Str}}
+             :handler (fn [{{{:keys [id]} :path} :parameters :keys [db]}]
+                        (-> (kayttaja-service/find-kayttaja db id)
+                            (api-response/get-response
+                             (str "Käyttäjä " id " does not exist."))))}}]
+     ["/laatija"
+      {:get {:summary "Hae käyttäjään liittyvät laatijatiedot"
+             :parameters {:path {:id common-schema/Key}}
+             :responses {200 {:body laatija-schema/Laatija}
+                         404 {:body schema/Str}}
+             :handler (fn [{{{:keys [id]} :path} :parameters :keys [db]}]
+                        (-> (laatija-service/find-laatija-with-kayttaja-id db id)
+                            (api-response/get-response
+                             (str "No laatija information for käyttäjä id " id))))}}]]]])
