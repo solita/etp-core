d608c0d4cdd381cddb649e161e205f54a305d522
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-03-31 14:57:14 +0300

    AE-101 change order of functions

--- etp-backend/src/main/clj/solita/etp/service/kayttaja.clj
+++ etp-backend/src/main/clj/solita/etp/service/kayttaja.clj
@@ -10,6 +10,32 @@
 ;; *** Conversions from database data types ***
 (def coerce-kayttaja (coerce/coercer kayttaja-schema/Kayttaja json/json-coercions))
 
+;;
+;; Roolit
+;;
+
+(def roolit [{:id 0
+              :label-fi "Laatija"
+              :label-sv "Laatija-SV"}
+             {:id 1
+              :label-fi "Pätevyyden toteaja"
+              :label-sv "Pätevyyden toteaja -SV"}
+             {:id 2
+              :label-fi "Pääkäyttäjä"
+              :label-sv "Pääkäyttäjä-SV"}])
+
+(defn find-roolit [] roolit)
+
+(defn patevyydentoteaja? [{:keys [role]}]
+  (= role 1))
+
+(defn paakayttaja? [{:keys [role]}]
+  (= role 2))
+
+;;
+;; Käyttäjä
+;;
+
 (defn find-kayttaja [db id]
   (->> {:id id}
        (kayttaja-db/select-kayttaja db)
@@ -30,21 +56,3 @@
 
 (defn update-kayttaja! [db id kayttaja]
   (kayttaja-db/update-kayttaja! db (assoc kayttaja :id id)))
-
-(def roolit [{:id 0
-              :label-fi "Laatija"
-              :label-sv "Laatija-SV"}
-             {:id 1
-              :label-fi "Pätevyyden toteaja"
-              :label-sv "Pätevyyden toteaja -SV"}
-             {:id 2
-              :label-fi "Pääkäyttäjä"
-              :label-sv "Pääkäyttäjä-SV"}])
-
-(defn find-roolit [] roolit)
-
-(defn patevyydentoteaja? [{:keys [role]}]
-  (= role 1))
-
-(defn paakayttaja? [{:keys [role]}]
-  (= role 2))
