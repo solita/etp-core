14bb130985576ae80da2107000fbbd71f0bb9c66
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-04-01 11:27:44 +0300

    AE-101 implement possibility to call add and update käyttäjä without logged in käyttäjä

--- etp-backend/src/main/clj/solita/etp/service/kayttaja.clj
+++ etp-backend/src/main/clj/solita/etp/service/kayttaja.clj
@@ -11,17 +11,17 @@
 ;; *** Conversions from database data types ***
 (def coerce-kayttaja (coerce/coercer kayttaja-schema/Kayttaja json/json-coercions))
 
-(defn find-kayttaja [db current-kayttaja id]
-  (when (or (= id (:id current-kayttaja))
-            (rooli-service/more-than-laatija? current-kayttaja))
+(defn find-kayttaja [db logged-in-kayttaja id]
+  (when (or (= id (:id logged-in-kayttaja))
+            (rooli-service/more-than-laatija? logged-in-kayttaja))
     (->> {:id id}
          (kayttaja-db/select-kayttaja db)
          (map coerce-kayttaja)
          first)))
 
-(defn find-kayttaja-with-email [db current-kayttaja email]
-  (when (or (= email (:email current-kayttaja))
-            (rooli-service/more-than-laatija? current-kayttaja))
+(defn find-kayttaja-with-email [db logged-in-kayttaja email]
+  (when (or (= email (:email logged-in-kayttaja))
+            (rooli-service/more-than-laatija? logged-in-kayttaja))
     (->> {:email email}
          (kayttaja-db/select-kayttaja-with-email db)
          (map coerce-kayttaja)
@@ -30,10 +30,17 @@
 (defn update-login! [db id cognitoid]
   (kayttaja-db/update-login! db {:id id :cognitoid cognitoid}))
 
-(defn add-kayttaja! [db kayttaja]
-  (:id (kayttaja-db/insert-kayttaja<! db kayttaja)))
+(defn add-kayttaja!
+  ([db kayttaja]
+   (:id (kayttaja-db/insert-kayttaja<! db kayttaja)))
+  ([db logged-in-kayttaja kayttaja]
+   (when (rooli-service/paakayttaja? logged-in-kayttaja)
+     (add-kayttaja! db kayttaja))))
 
-(defn update-kayttaja! [db current-kayttaja id kayttaja]
-  (when (or (= id (:id current-kayttaja))
-            (rooli-service/more-than-laatija? current-kayttaja))
-    (kayttaja-db/update-kayttaja! db (assoc kayttaja :id id))))
+(defn update-kayttaja!
+  ([db id kayttaja]
+   (kayttaja-db/update-kayttaja! db (assoc kayttaja :id id)))
+  ([db logged-in-kayttaja id kayttaja]
+   (when (or (= id (:id logged-in-kayttaja))
+             (rooli-service/paakayttaja? logged-in-kayttaja))
+     (update-kayttaja! db id kayttaja))))
