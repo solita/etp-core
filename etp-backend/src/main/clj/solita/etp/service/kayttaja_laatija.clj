d5414e70fa46c3571ce12bc101a101c91f85b752
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-04-07 14:48:48 +0300

    AE-124 remove email from schema when updating email

--- etp-backend/src/main/clj/solita/etp/service/kayttaja_laatija.clj
+++ etp-backend/src/main/clj/solita/etp/service/kayttaja_laatija.clj
@@ -53,8 +53,8 @@
 
 (defn update-kayttaja-laatija! [db whoami id kayttaja-laatija]
   (if (or (and (= id (:id whoami))
-                 (every? #(not (contains? kayttaja-laatija %))
-                         ks-only-for-paakayttaja))
+               (every? #(not (contains? kayttaja-laatija %))
+                       ks-only-for-paakayttaja))
             (rooli-service/paakayttaja? whoami))
     (let [kayttaja (st/select-schema kayttaja-laatija kayttaja-schema/KayttajaUpdate)
           laatija (st/select-schema kayttaja-laatija laatija-schema/LaatijaUpdate)]
