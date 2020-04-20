c14d32582ff687d3d1f89e3aacf31afaf8097d4f
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-04-20 11:47:20 +0300

    AE-100 add fields from energiatodistus to pdf for first three pages

--- etp-backend/src/main/clj/solita/etp/service/energiatodistus_pdf.clj
+++ etp-backend/src/main/clj/solita/etp/service/energiatodistus_pdf.clj
@@ -9,6 +9,10 @@
 (def sheet-count 8)
 (def tmp-dir "tmp/")
 
+(defn safe-str-* [x y]
+  (when (and x y)
+    (str (* x y))))
+
 (def mappings {0 {"K7" [:perustiedot :nimi]
                   "K8" [:perustiedot :katuosoite-fi]
 
@@ -24,15 +28,137 @@
                   ;; TODO checkboxes D19-D21
                   ;; TODO format date
                   "M21" [:perustiedot :havainnointikaynti]
+
                   ;; TODO M36 and M37 E-luku
                   ;; TODO laatija B42
 
                   "J42" [:perustiedot :yritys :nimi]
 
-                  ;; TODO real todistuksen laatimispäivä with correct formatting
                   "B50" (fn [_] (str (java.time.LocalDate/now)))
-                  ;; TODO voimassaolopäivä K50
-                  }})
+                  "K50" (fn [_] (str (.plusYears (java.time.LocalDate/now) 10)))}
+               1 {"F5" [:lahtotiedot :lammitetty-nettoala]
+                  "F6" [:lahtotiedot :lammitys :kuvaus-fi]
+                  "F7" [:lahtotiedot :ilmanvaihto :kuvaus-fi]
+
+                  ;; TODO vakioduilla käytöllä lasketut ostoenergiat
+                  ;; TODO Energiatehokkuuden vertailuluku
+
+                  ;; TODO Käytetty E-luvun luokittelu asteikko
+
+                  ;; TODO Luokkien rajat asteikolla
+
+                  ;; TODO Tämän rakennuksen energiatehokkuusluokka
+
+                  "C38" [:huomiot :suositukset-fi]}
+               2 {
+                  ;; TODO rakennuksen käyttötarkoitusluokka
+
+                  ;; TODO Rakennusvaippa; osuus lämpöhäviöstä
+                  "D5" [:perustiedot :valmistusmisvuosi]
+                  "F5" [:lahtotiedot :lammitetty-nettoala]
+                  "D7" [:lahtotiedot :rakennusvaippa :ilmanvaihtoluku]
+                  "D10" [:lahtotiedot :rakennusvaippa :ulkoseinat :ala]
+                  "E10" [:lahtotiedot :rakennusvaippa :ulkoseinat :U]
+                  "F10" #(safe-str-* (-> % :lahtotiedot :rakennusvaippa :ulkoseinat :ala)
+                                     (-> % :lahtotiedot :rakennusvaippa :ulkoseinat :U))
+                  "D11" [:lahtotiedot :rakennusvaippa :ylapohja :ala]
+                  "E11" [:lahtotiedot :rakennusvaippa :ylapohja :U]
+                  "F11" #(safe-str-* (-> % :lahtotiedot :rakennusvaippa :ylapohja :ala)
+                                     (-> % :lahtotiedot :rakennusvaippa :ylapohja :U))
+                  "D12" [:lahtotiedot :rakennusvaippa :alapohja :ala]
+                  "E12" [:lahtotiedot :rakennusvaippa :alapohja :U]
+                  "F12" #(safe-str-* (-> % :lahtotiedot :rakennusvaippa :alapohja :ala)
+                                     (-> % :lahtotiedot :rakennusvaippa :alapohja :U))
+                  "D13" [:lahtotiedot :rakennusvaippa :ikkunat :ala]
+                  "E13" [:lahtotiedot :rakennusvaippa :ikkunat :U]
+                  "F13" #(safe-str-* (-> % :lahtotiedot :rakennusvaippa :ikkunat :ala)
+                                     (-> % :lahtotiedot :rakennusvaippa :ikkunat :U))
+                  "D14" [:lahtotiedot :rakennusvaippa :ulkoovet :ala]
+                  "E14" [:lahtotiedot :rakennusvaippa :ulkoovet :U]
+                  "F14" #(safe-str-* (-> % :lahtotiedot :rakennusvaippa :ulkoovet :ala)
+                                     (-> % :lahtotiedot :rakennusvaippa :ulkoovet :U))
+                  "F15" [:lahtotiedot :rakennusvaippa :kylmasillat-UA]
+
+                  "D19" [:lahtotiedot :ikkunat :pohjoinen :ala]
+                  "E19" [:lahtotiedot :ikkunat :pohjoinen :U]
+                  "F19" [:lahtotiedot :ikkunat :pohjoinen :g-ks]
+                  "D20" [:lahtotiedot :ikkunat :koillinen :ala]
+                  "E20" [:lahtotiedot :ikkunat :koillinen :U]
+                  "F20" [:lahtotiedot :ikkunat :koillinen :g-ks]
+                  "D21" [:lahtotiedot :ikkunat :ita :ala]
+                  "E21" [:lahtotiedot :ikkunat :ita :U]
+                  "F21" [:lahtotiedot :ikkunat :ita :g-ks]
+                  "D22" [:lahtotiedot :ikkunat :kaakko :ala]
+                  "E22" [:lahtotiedot :ikkunat :kaakko :U]
+                  "F22" [:lahtotiedot :ikkunat :kaakko :g-ks]
+                  "D23" [:lahtotiedot :ikkunat :etela :ala]
+                  "E23" [:lahtotiedot :ikkunat :etela :U]
+                  "F23" [:lahtotiedot :ikkunat :etela :g-ks]
+                  "D24" [:lahtotiedot :ikkunat :lounas :ala]
+                  "E24" [:lahtotiedot :ikkunat :lounas :U]
+                  "F24" [:lahtotiedot :ikkunat :lounas :g-ks]
+                  "D25" [:lahtotiedot :ikkunat :lansi :ala]
+                  "E25" [:lahtotiedot :ikkunat :lansi :U]
+                  "F25" [:lahtotiedot :ikkunat :lansi :g-ks]
+                  "D26" [:lahtotiedot :ikkunat :luode :ala]
+                  "E26" [:lahtotiedot :ikkunat :luode :U]
+                  "F26" [:lahtotiedot :ikkunat :luode :g-ks]
+
+                  "D28" [:lahtotiedot :ilmanvaihto :kuvaus-fi]
+
+                  "D33" #(str (-> % :lahtotiedot :ilmanvaihto :paaiv :tulo)
+                              " / "
+                              (-> % :lahtotiedot :ilmanvaihto :paaiv :poisto))
+                  "E33" [:lahtotiedot :ilmanvaihto :paaiv :sfp]
+                  "F33" [:lahtotiedot :ilmanvaihto :paaiv :lampotilasuhde]
+                  "G33" [:lahtotiedot :ilmanvaihto :paaiv :jaatymisenesto]
+                  "D34" #(str (-> % :lahtotiedot :ilmanvaihto :erillispoistot :tulo)
+                              " / "
+                              (-> % :lahtotiedot :ilmanvaihto :erillispoistot :poisto))
+                  "E34" [:lahtotiedot :ilmanvaihto :erillispoistot :sfp]
+                  "D35" #(str (-> % :lahtotiedot :ilmanvaihto :ivjarjestelma :tulo)
+                              " / "
+                              (-> % :lahtotiedot :ilmanvaihto :ivjarjestelma :poisto))
+                  "E35" [:lahtotiedot :ilmanvaihto :ivjarjestelma :sfp]
+
+                  "E36" [:lahtotiedot :ilmanvaihto :lto-vuosihyotysuhde]
+
+                  "D38" [:lahtotiedot :lammitys :kuvaus-fi]
+
+                  "D43" [:lahtotiedot :lammitys :tilat-ja-iv :tuoton-hyotysuhde]
+                  "E43" [:lahtotiedot :lammitys :tilat-ja-iv :jaon-hyotysuhde]
+                  "F43" [:lahtotiedot :lammitys :tilat-ja-iv :lampokerroin]
+                  "G43" [:lahtotiedot :lammitys :tilat-ja-iv :apulaitteet]
+                  "D44" [:lahtotiedot :lammitys :lammin-kayttovesi :tuoton-hyotysuhde]
+                  "E44" [:lahtotiedot :lammitys :lammin-kayttovesi :jaon-hyotysuhde]
+                  "F44" [:lahtotiedot :lammitys :lammin-kayttovesi :lampokerroin]
+                  "G44" [:lahtotiedot :lammitys :lammin-kayttovesi :apulaitteet]
+
+                  "D50" [:lahtotiedot :lammitys :takka :maara]
+                  "E50" [:lahtotiedot :lammitys :takka :tuotto]
+                  "D51" [:lahtotiedot :lammitys :ilmanlampopumppu :maara]
+                  "E51" [:lahtotiedot :lammitys :ilmanlampopumppu :tuotto]
+
+                  "D55" [:lahtotiedot :jaahdytysjarjestelma :jaahdytyskauden-painotettu-kylmakerroin]
+
+                  ;; TODO lämmin käyttövesi ominaiskulutus, lämmitysenergian nettotarve
+
+                  "C63" [:lahtotiedot :sis-kuorma 0 :selite-fi]
+                  "D63" [:lahtotiedot :sis-kuorma 0 :kayttoaste]
+                  "E63" [:lahtotiedot :sis-kuorma 0 :henkilot]
+                  "F63" [:lahtotiedot :sis-kuorma 0 :kuluttajalaitteet]
+                  "G63" [:lahtotiedot :sis-kuorma 0 :valaistus]
+                  "C64" [:lahtotiedot :sis-kuorma 1 :selite-fi]
+                  "D64" [:lahtotiedot :sis-kuorma 1 :kayttoaste]
+                  "E64" [:lahtotiedot :sis-kuorma 1 :henkilot]
+                  "F64" [:lahtotiedot :sis-kuorma 1 :kuluttajalaitteet]
+                  "G64" [:lahtotiedot :sis-kuorma 1 :valaistus]
+                  "C65" [:lahtotiedot :sis-kuorma 2 :selite-fi]
+                  "D65" [:lahtotiedot :sis-kuorma 2 :kayttoaste]
+                  "E65" [:lahtotiedot :sis-kuorma 2 :henkilot]
+                  "F65" [:lahtotiedot :sis-kuorma 2 :kuluttajalaitteet]
+                  "G66" [:lahtotiedot :sis-kuorma 2 :valaistus]}
+               })
 
 (defn fill-xlsx-template [energiatodistus]
   (with-open [is (-> xlsx-template-path io/resource io/input-stream)]
