83ca047c9a49b5c4a902126151660f6152bb5570
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-05-15 14:54:26 +0300

    AE-250 fix henkilötunnukset of laatija once again

--- etp-db/src/test/sql/migration/v2-laatija/v2.4-test-laatijat.sql
+++ etp-db/src/test/sql/migration/v2-laatija/v2.4-test-laatijat.sql
@@ -5,12 +5,12 @@ SELECT id, id, '100183-563U', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33
 union all
 SELECT id, id, '010100-1111', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ulla.laatija@example.com'
 union all
-SELECT id, id, '01010100-222L', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'kirsi.laatija@example.com'
+SELECT id, id, '010100-222L', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'kirsi.laatija@example.com'
 union all
-SELECT id, id, '01010100-3336', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ari.laatija@example.com'
+SELECT id, id, '010100-3336', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ari.laatija@example.com'
 union all
-SELECT id, id, '01010100-444S', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'risto.laatija@example.com'
+SELECT id, id, '010100-444S', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'risto.laatija@example.com'
 union all
-SELECT id, id, '01010100-555B', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'marko.laatija@example.com'
+SELECT id, id, '010100-555B', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'marko.laatija@example.com'
 union all
-SELECT id, id, '01010100-666X', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'aleksi.laatija@example.com';
+SELECT id, id, '010100-666X', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'aleksi.laatija@example.com';
