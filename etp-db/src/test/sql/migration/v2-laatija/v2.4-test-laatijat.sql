2a35135006a893947c8bddcb0143a395a9daa629
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-05-15 14:51:14 +0300

    AE-250 add valid henkilötunnus to test laatijat

--- etp-db/src/test/sql/migration/v2-laatija/v2.4-test-laatijat.sql
+++ etp-db/src/test/sql/migration/v2-laatija/v2.4-test-laatijat.sql
@@ -3,14 +3,14 @@ SELECT id, id, '271190-836F', 1, date '2020-01-01', 'KIINKO', 'Hämeenkatu 1', '
 union all
 SELECT id, id, '100183-563U', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'laatija2@example.com'
 union all
-SELECT id, id, '01010100-111A', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ulla.laatija@example.com'
+SELECT id, id, '010100-1111', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ulla.laatija@example.com'
 union all
-SELECT id, id, '01010100-111B', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'kirsi.laatija@example.com'
+SELECT id, id, '01010100-222L', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'kirsi.laatija@example.com'
 union all
-SELECT id, id, '01010100-111C', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ari.laatija@example.com'
+SELECT id, id, '01010100-3336', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ari.laatija@example.com'
 union all
-SELECT id, id, '01010100-111D', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'risto.laatija@example.com'
+SELECT id, id, '01010100-444S', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'risto.laatija@example.com'
 union all
-SELECT id, id, '01010100-111E', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'marko.laatija@example.com'
+SELECT id, id, '01010100-555B', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'marko.laatija@example.com'
 union all
-SELECT id, id, '01010100-111F', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'aleksi.laatija@example.com';
+SELECT id, id, '01010100-666X', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'aleksi.laatija@example.com';
