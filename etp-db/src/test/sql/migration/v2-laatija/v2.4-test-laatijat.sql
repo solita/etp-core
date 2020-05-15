0d03bb4573aff12e1dd1aee14c35cd6f37a02f46
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-05-15 14:34:40 +0300

    AE-250 add missing laatijat to db

--- etp-db/src/test/sql/migration/v2-laatija/v2.4-test-laatijat.sql
+++ etp-db/src/test/sql/migration/v2-laatija/v2.4-test-laatijat.sql
@@ -1,4 +1,16 @@
 INSERT INTO laatija (id, kayttaja, henkilotunnus, patevyystaso, toteamispaivamaara, toteaja, jakeluosoite, postinumero, postitoimipaikka)
 SELECT id, id, '271190-836F', 1, date '2020-01-01', 'KIINKO', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'laatija1@example.com'
 union all
-SELECT id, id, '100183-563U', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'laatija2@example.com';
+SELECT id, id, '100183-563U', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'laatija2@example.com'
+union all
+SELECT id, id, '01010100-111A', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ulla.laatija@example.com'
+union all
+SELECT id, id, '01010100-111B', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'kirsi.laatija@example.com'
+union all
+SELECT id, id, '01010100-111C', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ari.laatija@example.com'
+union all
+SELECT id, id, '01010100-111D', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'risto.laatija@example.com'
+union all
+SELECT id, id, '01010100-111E', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'marko.laatija@example.com'
+union all
+SELECT id, id, '01010100-111F', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'aleksi.laatija@example.com';
