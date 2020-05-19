0c4e3cc85e8c97a01e0b33bc70d706ad395cb7bb
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-05-19 14:31:56 +0300

    AE-165 - connect system shutdown to java runtime shutdown hook

--- etp-backend/src/main/clj/solita/etp/core.clj
+++ etp-backend/src/main/clj/solita/etp/core.clj
@@ -1,5 +1,9 @@
 (ns solita.etp.core
   (:require [solita.etp.system :as system]))
 
+(defn add-shutdown-hook! [f]
+  (.addShutdownHook (Runtime/getRuntime) (Thread. f)))
+
 (defn -main []
-  (system/start!))
+  (let [system (system/start!)]
+    (add-shutdown-hook! #(system/halt! system))))
