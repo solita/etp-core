614abd95a279f8d04475bf8cf5e9d2ff21efa1b0
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2020-05-29 09:16:55 +0300

    AE-210: set suomi.fi test user's hetu for laatija2

--- etp-db/src/test/sql/migration/v2-laatija/v2.4-test-laatijat.sql
+++ etp-db/src/test/sql/migration/v2-laatija/v2.4-test-laatijat.sql
@@ -1,7 +1,7 @@
 INSERT INTO laatija (id, kayttaja, henkilotunnus, patevyystaso, toteamispaivamaara, toteaja, jakeluosoite, postinumero, postitoimipaikka)
 SELECT id, id, '271190-836F', 1, date '2020-01-01', 'KIINKO', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'laatija1@example.com'
 union all
-SELECT id, id, '100183-563U', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'laatija2@example.com'
+SELECT id, id, '210281-9988', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'laatija2@example.com'
 union all
 SELECT id, id, '010100-1111', 2, date '2019-01-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email = 'ulla.laatija@example.com'
 union all
