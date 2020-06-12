ad132beb5669b6d4d259c5211addc5814111ae42
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-06-12 12:05:14 +0300

    AE-327 implement functions to get e-luokka with e-luku and nettoala

--- etp-backend/src/main/clj/solita/etp/service/e_luokka.clj
+++ etp-backend/src/main/clj/solita/etp/service/e_luokka.clj
@@ -1,4 +1,22 @@
 (ns solita.etp.service.e-luokka)
 
+(defn limit [nettoala x kerroin]
+  (->> nettoala (* kerroin) (- x) int))
+
+(defn e-luokka-from-e-luku-and-nettoala [e-luku nettoala limits default-luokka]
+  (or (some (fn [[x kerroin e-luokka]]
+              (if (<= e-luku (limit nettoala x kerroin))
+                e-luokka))
+            limits)
+      default-luokka))
+
+(defn pienet-asuinrakennukset-50-150-2018 [e-luku nettoala]
+  (e-luokka-from-e-luku-and-nettoala
+   e-luku
+   nettoala
+   [[110 0.2 "A"] [215 0.6 "B"] [252 0.6 "C"] [332 0.6 "D"] [462 0.6 "E"]
+    [532 0.6 "F"]]
+   "G"))
+
 (defn find-e-luokka [db versio alakayttotarkoitusluokka nettoala e-luku]
   {:e-luokka "A"})
--- etp-backend/src/test/clj/solita/etp/service/e_luokka_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/e_luokka_test.clj
@@ -3,5 +3,25 @@
             [solita.etp.test-system :as ts]
             [solita.etp.service.e-luokka :as service]))
 
+(t/deftest limit-test
+  (t/is (= 125 (service/limit 150 215 0.6)))
+  (t/is (= 124 (service/limit 150.5 215 0.6))))
+
+(t/deftest e-luokka-from-e-luku-and-nettoala-test
+  (t/is (= "C" (service/e-luokka-from-e-luku-and-nettoala
+                100
+                100
+                [[100 2 "A"] [200 2 "B"] [300 2 "C"] [400 2 "D"]]
+                "E")))
+  (t/is (= "B" (service/e-luokka-from-e-luku-and-nettoala
+                100
+                100
+                [[100 2 "A"]]
+                "B"))))
+
+(t/deftest pienet-asuinrakennukset-50-150-2018
+  (t/is (= "B" (service/pienet-asuinrakennukset-50-150-2018 125 150)))
+  (t/is (= "C" (service/pienet-asuinrakennukset-50-150-2018 126 150))))
+
 (t/deftest find-e-luokka-test
   (t/is (= {:e-luokka "A"} (service/find-e-luokka ts/*db* 2018 "ABC" 100 120))))
