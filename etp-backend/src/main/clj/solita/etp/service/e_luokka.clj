3a3a3d88ffbad5201a85b2f7e82136ba60669a46
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-06-12 12:50:02 +0300

    AE-327 implement support for all 2018 tables for e-luokka by e-luku

--- etp-backend/src/main/clj/solita/etp/service/e_luokka.clj
+++ etp-backend/src/main/clj/solita/etp/service/e_luokka.clj
@@ -36,5 +36,59 @@
    [[70 "A"] [106 "B"] [130 "C"] [210 "D"] [340 "E"] [410 "F"]]
    "G"))
 
+(defn rivitalot-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[80 "A"] [110 "B"] [150 "C"] [210 "D"] [340 "E"] [410 "F"]]
+   "G"))
+
+(defn asuinkerrostalot-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[75 "A"] [100 "B"] [130 "C"] [160 "D"] [190 "E"] [240 "F"]]
+   "G"))
+
+(defn toimistorakennukset-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[80 "A"] [120 "B"] [170 "C"] [200 "D"] [240 "E"] [300 "F"]]
+   "G"))
+
+(defn liikerakennukset-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[90 "A"] [170 "B"] [240 "C"] [280 "D"] [340 "E"] [390 "F"]]
+   "G"))
+
+(defn majoitusliikerakennukset-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[90 "A"] [170 "B"] [240 "C"] [280 "D"] [340 "E"] [450 "F"]]
+   "G"))
+
+(defn opetusrakennukset-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[90 "A"] [130 "B"] [170 "C"] [230 "D"] [300 "E"] [360 "F"]]
+   "G"))
+
+(defn liikuntahallit-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[90 "A"] [130 "B"] [170 "C"] [190 "D"] [240 "E"] [280 "F"]]
+   "G"))
+
+(defn sairaalat-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[150 "A"] [350 "B"] [450 "C"] [550 "D"] [650 "E"] [800 "F"]]
+   "G"))
+
+(defn muut-2018 [e-luku _]
+  (e-luokka-from-e-luku
+   e-luku
+   [[90 "A"] [130 "B"] [170 "C"] [190 "D"] [240 "E"] [280 "F"]]
+   "G"))
+
 (defn find-e-luokka [db versio alakayttotarkoitusluokka nettoala e-luku]
   {:e-luokka "A"})
