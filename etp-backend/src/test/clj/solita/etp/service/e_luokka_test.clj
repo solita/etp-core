28bc829c3e4e9bcbefcc5f32081de3b4a21c56f6
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-06-12 16:31:57 +0300

    AE-327 fix tests to support renamed e-luokka functions

--- etp-backend/src/test/clj/solita/etp/service/e_luokka_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/e_luokka_test.clj
@@ -24,18 +24,18 @@
   (t/is (= "B" (service/pienet-asuinrakennukset-50-150-2018 125 150)))
   (t/is (= "C" (service/pienet-asuinrakennukset-50-150-2018 126 150))))
 
-(t/deftest pienet-asuinrakennukset-150-600-2018-test
-  (t/is (= "F" (service/pienet-asuinrakennukset-150-600-2018 411 600)))
-  (t/is (= "G" (service/pienet-asuinrakennukset-150-600-2018 412 600))))
+(t/deftest pienet-asuinrakennukset-150-600-2013-2018-test
+  (t/is (= "F" (service/pienet-asuinrakennukset-150-600-2013-2018 411 600)))
+  (t/is (= "G" (service/pienet-asuinrakennukset-150-600-2013-2018 412 600))))
 
-(t/deftest pienet-asuinrakennukset-600-2018-test
-  (t/is (= "A" (service/pienet-asuinrakennukset-600-2018 70 800)))
-  (t/is (= "B" (service/pienet-asuinrakennukset-600-2018 106 800)))
-  (t/is (= "C" (service/pienet-asuinrakennukset-600-2018 130 800)))
-  (t/is (= "D" (service/pienet-asuinrakennukset-600-2018 210 800)))
-  (t/is (= "E" (service/pienet-asuinrakennukset-600-2018 340 800)))
-  (t/is (= "F" (service/pienet-asuinrakennukset-600-2018 410 800)))
-  (t/is (= "G" (service/pienet-asuinrakennukset-600-2018 1000 800))))
+(t/deftest pienet-asuinrakennukset-600-2013-2018-test
+  (t/is (= "A" (service/pienet-asuinrakennukset-600-2013-2018 70 800)))
+  (t/is (= "B" (service/pienet-asuinrakennukset-600-2013-2018 106 800)))
+  (t/is (= "C" (service/pienet-asuinrakennukset-600-2013-2018 130 800)))
+  (t/is (= "D" (service/pienet-asuinrakennukset-600-2013-2018 210 800)))
+  (t/is (= "E" (service/pienet-asuinrakennukset-600-2013-2018 340 800)))
+  (t/is (= "F" (service/pienet-asuinrakennukset-600-2013-2018 410 800)))
+  (t/is (= "G" (service/pienet-asuinrakennukset-600-2013-2018 1000 800))))
 
 (t/deftest find-e-luokka-test
   (t/is (= {:e-luokka "A"} (service/find-e-luokka ts/*db* 2018 "ABC" 100 120))))
