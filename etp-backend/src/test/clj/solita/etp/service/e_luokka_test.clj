7d9de2aaba67ba23d781c04eb803c9dd3626afd5
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-06-15 16:07:32 +0300

    AE-327 implement choosing the correct e-luokka function

--- etp-backend/src/test/clj/solita/etp/service/e_luokka_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/e_luokka_test.clj
@@ -3,6 +3,8 @@
             [solita.etp.test-system :as ts]
             [solita.etp.service.e-luokka :as service]))
 
+(t/use-fixtures :each ts/fixture)
+
 (t/deftest e-luokka-from-e-luku-and-nettoala-test
   (let [limits [[100 2 "A"] [200 2 "B"] [300 2 "C"] [400 2 "D"]]]
     (t/is (= "C" (service/e-luokka-from-e-luku-and-nettoala 100
@@ -38,4 +40,22 @@
   (t/is (= "G" (service/pienet-asuinrakennukset-600-2013-2018 1000 800))))
 
 (t/deftest find-e-luokka-test
-  (t/is (= {:e-luokka "A"} (service/find-e-luokka ts/*db* 2018 "ABC" 100 120))))
+  (t/is (= {:e-luokka "A"} (service/find-e-luokka ts/*db* 2013 "YAT" 100 1)))
+  (t/is (= {:e-luokka "A"} (service/find-e-luokka ts/*db* 2018 "KAT" 100 1)))
+  (t/is (= {:e-luokka "B"} (service/find-e-luokka ts/*db* 2013 "MAEP" 100 95)))
+  (t/is (= {:e-luokka "D"} (service/find-e-luokka ts/*db* 2013 "RK" 100 151)))
+  (t/is (= {:e-luokka "D"} (service/find-e-luokka ts/*db* 2018 "RT" 100 151)))
+  (t/is (= {:e-luokka "D"} (service/find-e-luokka ts/*db* 2018 "AK3" 100 151)))
+  (t/is (= {:e-luokka "E"} (service/find-e-luokka ts/*db* 2013 "T" 100 240)))
+  (t/is (= {:e-luokka "E"} (service/find-e-luokka ts/*db* 2018 "TE" 100 240)))
+  (t/is (= {:e-luokka "F"} (service/find-e-luokka ts/*db* 2013 "TOKK" 100 380)))
+  (t/is (= {:e-luokka "F"} (service/find-e-luokka ts/*db* 2018 "MH" 100 380)))
+  (t/is (= {:e-luokka "C"} (service/find-e-luokka ts/*db* 2013 "N" 100 240)))
+  (t/is (= {:e-luokka "C"} (service/find-e-luokka ts/*db* 2018 "N" 100 240)))
+  (t/is (= {:e-luokka "D"} (service/find-e-luokka ts/*db* 2013 "AOR" 100 230)))
+  (t/is (= {:e-luokka "D"} (service/find-e-luokka ts/*db* 2018 "OR" 100 230)))
+  (t/is (= {:e-luokka "G"} (service/find-e-luokka ts/*db* 2013 "TSSJ" 100 282)))
+  (t/is (= {:e-luokka "G"} (service/find-e-luokka ts/*db* 2018 "LH" 100 282)))
+  (t/is (= {:e-luokka "E"} (service/find-e-luokka ts/*db* 2013 "MS" 100 650)))
+  (t/is (= {:e-luokka "E"} (service/find-e-luokka ts/*db* 2018 "S" 100 650)))
+  (t/is (= {:e-luokka "B"} (service/find-e-luokka ts/*db* 2018 "JH" 100 130))))
