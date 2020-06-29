beb453cc2ba221bd57583deccda40c36aae71737
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-06-29 12:29:43 +0300

    AE-293 implement first decimal place configurations to pdf cell mappings

--- etp-backend/src/main/clj/solita/etp/service/energiatodistus_pdf.clj
+++ etp-backend/src/main/clj/solita/etp/service/energiatodistus_pdf.clj
@@ -97,26 +97,26 @@
                       (str " m²"))}
         "F6" {:path [:lahtotiedot :lammitys :kuvaus-fi]}
         "F7" {:path [:lahtotiedot :ilmanvaihto :kuvaus-fi]}
-        "F14" {:path [:tulokset :kaytettavat-energiamuodot :kaukolampo]}
-        "G14" {:path [:tulokset :kaytettavat-energiamuodot :kaukolampo-nettoala]}
+        "F14" {:path [:tulokset :kaytettavat-energiamuodot :kaukolampo] :dp 0}
+        "G14" {:path [:tulokset :kaytettavat-energiamuodot :kaukolampo-nettoala] :dp 0}
         "H14" {:path [:tulokset :kaytettavat-energiamuodot :kaukolampo-kerroin]}
-        "I14" {:path [:tulokset :kaytettavat-energiamuodot :kaukolampo-nettoala-kertoimella]}
-        "F15" {:path [:tulokset :kaytettavat-energiamuodot :sahko]}
-        "G15" {:path [:tulokset :kaytettavat-energiamuodot :sahko-nettoala]}
+        "I14" {:path [:tulokset :kaytettavat-energiamuodot :kaukolampo-nettoala-kertoimella :dp 0]}
+        "F15" {:path [:tulokset :kaytettavat-energiamuodot :sahko] :dp 0}
+        "G15" {:path [:tulokset :kaytettavat-energiamuodot :sahko-nettoala] :dp 0}
         "H15" {:path [:tulokset :kaytettavat-energiamuodot :sahko-kerroin]}
-        "I15" {:path [:tulokset :kaytettavat-energiamuodot :sahko-nettoala-kertoimella]}
-        "F16" {:path [:tulokset :kaytettavat-energiamuodot :uusiutuva-polttoaine]}
-        "G16" {:path [:tulokset :kaytettavat-energiamuodot :uusiutuva-polttoaine-nettoala]}
+        "I15" {:path [:tulokset :kaytettavat-energiamuodot :sahko-nettoala-kertoimella] :dp 0}
+        "F16" {:path [:tulokset :kaytettavat-energiamuodot :uusiutuva-polttoaine] :dp 0}
+        "G16" {:path [:tulokset :kaytettavat-energiamuodot :uusiutuva-polttoaine-nettoala] :dp 0}
         "H16" {:path [:tulokset :kaytettavat-energiamuodot :uusiutuva-polttoaine-kerroin]}
-        "I16" {:path [:tulokset :kaytettavat-energiamuodot :uusiutuva-polttoaine-nettoala-kertoimella]}
-        "F17" {:path [:tulokset :kaytettavat-energiamuodot :fossiilinen-polttoaine]}
-        "G17" {:path [:tulokset :kaytettavat-energiamuodot :fossiilinen-polttoaine-nettoala]}
+        "I16" {:path [:tulokset :kaytettavat-energiamuodot :uusiutuva-polttoaine-nettoala-kertoimella] :dp 0}
+        "F17" {:path [:tulokset :kaytettavat-energiamuodot :fossiilinen-polttoaine] :dp 0}
+        "G17" {:path [:tulokset :kaytettavat-energiamuodot :fossiilinen-polttoaine-nettoala] :dp 0}
         "H17" {:path [:tulokset :kaytettavat-energiamuodot :fossiilinen-polttoaine-kerroin]}
-        "I17" {:path [:tulokset :kaytettavat-energiamuodot :fossiilinen-polttoaine-nettoala-kertoimella]}
-        "F18" {:path [:tulokset :kaytettavat-energiamuodot :kaukojaahdytys]}
-        "G18" {:path [:tulokset :kaytettavat-energiamuodot :kaukojaahdytys-nettoala]}
+        "I17" {:path [:tulokset :kaytettavat-energiamuodot :fossiilinen-polttoaine-nettoala-kertoimella] :dp 0}
+        "F18" {:path [:tulokset :kaytettavat-energiamuodot :kaukojaahdytys] :dp 0}
+        "G18" {:path [:tulokset :kaytettavat-energiamuodot :kaukojaahdytys-nettoala] :dp 0}
         "H18" {:path [:tulokset :kaytettavat-energiamuodot :kaukojaahdytys-kerroin]}
-        "I18" {:path [:tulokset :kaytettavat-energiamuodot :kaukojaahdytys-nettoala-kertoimella]}
+        "I18" {:path [:tulokset :kaytettavat-energiamuodot :kaukojaahdytys-nettoala-kertoimella] :dp 0}
 
         "I20" {:path [:tulokset :e-luku]}
 
@@ -449,12 +449,15 @@
                     (format "energiatodistus-%s.xlsx")
                     (str tmp-dir))]
       (doseq [[sheet sheet-mappings] (mappings)]
-        (doseq [[cell {:keys [path f]}] sheet-mappings]
+        (doseq [[cell {:keys [path f dp percent?]}] sheet-mappings]
           (xlsx/set-cell-value-at (nth sheets sheet)
                                   cell
                                   (if f
                                     (f complete-energiatodistus)
-                                    (get-in complete-energiatodistus path)))))
+                                    (let [v (get-in complete-energiatodistus path)]
+                                      (if (number? v)
+                                        (format-number v dp percent?)
+                                        v))))))
       (io/make-parents path)
       (xlsx/save-xlsx loaded-xlsx path)
       path)))
