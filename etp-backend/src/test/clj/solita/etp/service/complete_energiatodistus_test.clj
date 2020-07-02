f5ec5e578aecf676bfd78ee70a2898e527ec2e1e
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-07-02 11:08:56 +0300

    AE-277 add basic tests for complete energiatodistus

---
+++ etp-backend/src/test/clj/solita/etp/service/complete_energiatodistus_test.clj
@@ -0,0 +1,47 @@
+(ns solita.etp.service.complete-energiatodistus-test
+  (:require [clojure.test :as t]
+            [solita.etp.test-system :as ts]
+            [solita.etp.service.complete-energiatodistus :as service]
+            [solita.etp.service.energiatodistus-test :as energiatodistus-test]))
+
+(t/use-fixtures :each ts/fixture)
+
+(def energiatodistus (->
+                      (energiatodistus-test/generate-energiatodistus-2018)
+                      (assoc-in [:tulokset
+                                 :kaytettavat-energiamuodot
+                                 :kaukolampo]
+                                1500)
+                      (assoc-in [:lahtotiedot :ilmanvaihto :paaiv :tulo] 15)
+                      (assoc-in [:lahtotiedot :ilmanvaihto :paaiv :poisto] 35)))
+
+(defn assert-complete-energiatoditus [complete-energiatodistus]
+  (and (= 750.0 (-> complete-energiatodistus
+                    :tulokset
+                    :kaytettavat-energiamuodot
+                    :kaukolampo-kertoimella))
+       (= "15 / 35" (-> complete-energiatodistus
+                        :lahtotiedot
+                        :ilmanvaihto
+                        :paaiv
+                        :tulo-poisto))))
+
+(t/deftest find-complete-energiatodistus-test
+  (let [laatija-id (energiatodistus-test/add-laatija!)
+        id (energiatodistus-test/add-energiatodistus! energiatodistus laatija-id)
+        whoami {:id laatija-id :rooli 0}
+        complete-energiatodistus (service/find-complete-energiatodistus ts/*db*
+                                                                        whoami
+                                                                        id)]
+    (t/is (assert-complete-energiatoditus complete-energiatodistus))))
+
+(t/deftest find-complete-energiatodistukset-by-laatija-test
+  (let [laatija-id (energiatodistus-test/add-laatija!)
+        _ (energiatodistus-test/add-energiatodistus! energiatodistus laatija-id)
+        _ (energiatodistus-test/add-energiatodistus! energiatodistus laatija-id)
+        complete-energiatodistukset (service/find-complete-energiatodistukset-by-laatija
+                                     ts/*db*
+                                     laatija-id
+                                     nil)]
+    (t/is (= 2 (count complete-energiatodistukset)))
+    (t/is (every? assert-complete-energiatoditus complete-energiatodistukset))))
