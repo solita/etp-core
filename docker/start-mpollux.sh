155ac6500a373d137b41f8e23094572d6ea79bc6
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-08-17 10:10:10 +0300

    add mpollux to docker-compose + publish ports to localhost only

--- docker/start-mpollux.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd $(dirname $0)
-
-docker run --name mpollux -p 127.0.0.1:53952:443 \
-  -v `pwd`/mpollux/api:/usr/share/nginx/html:ro \
-  -v `pwd`/mpollux/conf.d:/etc/nginx/conf.d:ro \
-  -v `pwd`/../../etp-front/keys:/keys:ro \
-  -d nginx
