d826ef772dbdd043ca1fe9c1bfa2f50e475bd35b
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-08-31 09:09:43 +0300

    AE-446 add identifiers to db default opts and use it in luokittelu service

--- etp-backend/src/main/clj/solita/etp/service/luokittelu.clj
+++ etp-backend/src/main/clj/solita/etp/service/luokittelu.clj
@@ -3,7 +3,9 @@
             [solita.etp.db :as db]))
 
 (defn- select-luokittelu [db table-name]
-  (jdbc/query db [(format "SELECT id, label_fi \"label-fi\", label_sv \"label-sv\", valid FROM %s" table-name)]))
+  (jdbc/query db
+              [(format "SELECT id, label_fi, label_sv, valid FROM %s" table-name)]
+              db/default-opts))
 
 (def find-ilmanvaihtotyypit #(select-luokittelu % "ilmanvaihtotyyppi"))
 (def find-lammitysmuodot #(select-luokittelu % "lammitysmuoto"))
