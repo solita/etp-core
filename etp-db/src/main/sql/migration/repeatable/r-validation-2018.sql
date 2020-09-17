0e361b8f8e0344b6e88ad6df8854114ee8e865db
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-09-17 14:55:07 +0300

    AE-137 - all numeric limits from excel - refactor limits to a separate file

--- etp-db/src/main/sql/migration/repeatable/r-validation-2018.sql
+++ etp-db/src/main/sql/migration/repeatable/r-validation-2018.sql
@@ -1,19 +1,4 @@
 
-insert into validation_numeric_column
-  (versio, column_name, warning$min, warning$max, error$min, error$max)
-values
-(2018, 'lt$rakennusvaippa$ulkoseinat$U', 0.08, 0.81, 0.05, 2),
-(2018, 'lt$rakennusvaippa$ylapohja$U',   0.05, 0.47, 0.03, 2),
-(2018, 'lt$rakennusvaippa$alapohja$U',   0.05, 0.60, 0.03, 4),
-(2018, 'lt$rakennusvaippa$ikkunat$U',    0.05, 3.10, 0.04, 6.5),
-(2018, 'lt$rakennusvaippa$ulkoovet$U',   0.08, 0.81, 0.05, 2)
-on conflict (column_name, versio) do update
-  set
-    warning$min = excluded.warning$min,
-    warning$max = excluded.warning$max,
-    error$min = excluded.error$min,
-    error$max = excluded.error$max;
-
 insert into validation_required_column (versio, column_name, ordinal)
 values
 (2018, 'pt$kieli', 0),
