ca1ae67a41700f1b7743d5071158539712daddf7
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2020-09-25 15:12:39 +0300

    show env

--- etp-backend/src/main/clj/solita/common/aws.clj
+++ etp-backend/src/main/clj/solita/common/aws.clj
@@ -7,8 +7,7 @@
 
 (defonce s3 (aws/client {:api                  :s3}))
 
-(jwt/http-get "http://169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"
-          #(-> % println))
+(println (System/getenv))
 
 (defn put-object [file-name, bytes]
   (aws/invoke s3 {:op      :PutObject
