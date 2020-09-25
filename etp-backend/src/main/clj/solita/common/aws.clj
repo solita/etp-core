f024fd70ba356c77dd825d9e63b4ab92972412a1
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2020-09-25 14:47:31 +0300

    check credentials

--- etp-backend/src/main/clj/solita/common/aws.clj
+++ etp-backend/src/main/clj/solita/common/aws.clj
@@ -1,14 +1,17 @@
 (ns solita.common.aws
   (:require [cognitect.aws.client.api :as aws]
+            [solita.etp.jwt :as jwt]
             [cognitect.aws.credentials :as credentials]))
 
 (def bucket-name "etp-energiatodistuksen-tiedostot-dev")
 
 (defonce s3 (aws/client {:api                  :s3}))
 
+(jwt/http-get "http://169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"
+          #(-> % println))
+
 (defn put-object [file-name, bytes]
   (aws/invoke s3 {:op      :PutObject
-                  :credentials-provider (credentials/container-credentials-provider)
                   :request {:Bucket bucket-name
                             :Key    file-name
                             :Body   bytes}}))
