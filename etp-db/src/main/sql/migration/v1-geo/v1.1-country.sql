75418098ae43a0253ba83de6506231e4be27bc51
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-09-29 10:01:11 +0300

    AE-461 - geo location data

--- etp-db/src/main/sql/migration/v1-geo/v1.1-country.sql
+++ etp-db/src/main/sql/migration/v1-geo/v1.1-country.sql
@@ -2,10 +2,12 @@
 -- see https://en.wikipedia.org/wiki/Country_code
 
 create table country (
-    id char(2) primary key,
-    alpha3 char(3),
-    numeric int,
-    name text,
-    label_fi text,
-    label_sv text
+  id char(2) primary key,
+  alpha3 char(3),
+  numeric int,
+  name text,
+  label_fi text,
+  label_sv text,
+  ordinal int not null default 0,
+  valid boolean not null default true
 );
