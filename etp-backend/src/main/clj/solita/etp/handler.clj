e6ee027f53d7ee35c54c16d4a6f436214ad7987e
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-09-29 15:53:08 +0300

    AE-496 create a new routing path with separate middleware for public

--- etp-backend/src/main/clj/solita/etp/handler.clj
+++ etp-backend/src/main/clj/solita/etp/handler.clj
@@ -65,6 +65,16 @@
 (def routes
   ["/api"
    system-routes
+   ["/public" {:middleware [[security/wrap-public]
+                            [security/wrap-db-application-name]]}
+    ["/test"
+     {:get {:summary "Test endpoint"
+            :tags #{"test"}
+            :handler (fn [{:keys [whoami public? db]}]
+                       {:status 200
+                        :body {:whoami whoami
+                               :public? public?
+                               :db (str db)}})}}]]
    ["/private" {:middleware [[security/wrap-jwt-payloads]
                              [security/wrap-whoami-from-jwt-payloads]
                              [security/wrap-access]
@@ -84,8 +94,7 @@
    ;;:reitit.middleware/transform dev/print-request-diffs
    :exception pretty/exception
    :validate rs/validate
-   :data {
-          :coercion reitit.coercion.schema/coercion
+   :data {:coercion reitit.coercion.schema/coercion
           :muuntaja m/instance
           :middleware [swagger/swagger-feature
                        parameters/parameters-middleware
@@ -108,4 +117,3 @@
                            :config {:validationUrl nil}
                            :operationsSorter "alpha"})
                         (ring/create-default-handler))))
-
