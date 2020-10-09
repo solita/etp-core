68195e97f13afed777c60c1921eb3db331060b7d
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-10-09 11:18:35 +0300

    AE-139 implement support for api key to whoami service

--- etp-backend/src/main/clj/solita/etp/service/whoami.clj
+++ etp-backend/src/main/clj/solita/etp/service/whoami.clj
@@ -1,5 +1,8 @@
 (ns solita.etp.service.whoami
-  (:require [schema.coerce :as coerce]
+  (:require [clojure.core.match :as match]
+            [schema.core :as schema]
+            [schema-tools.core :as st]
+            [buddy.hashers :as hashers]
             [flathead.flatten :as flat]
             [solita.etp.db :as db]
             [solita.etp.service.json :as json]
@@ -8,10 +11,12 @@
 ;; *** Require sql functions ***
 (db/require-queries 'whoami)
 
-;; *** Conversions from database data types ***
-(def coerce-whoami (coerce/coercer whoami-schema/Whoami json/json-coercions))
+(defn verified-api-key? [api-key api-key-hash]
+  (if (and api-key api-key-hash)
+    (:valid (hashers/verify api-key api-key-hash))
+    false))
 
-(defn find-whoami [db opts]
+(defn- find-whoami-with-api-key-hash [db opts]
   (->> (merge {:email nil
                :cognitoid nil
                :henkilotunnus nil
@@ -20,9 +25,17 @@
               opts)
        (flat/tree->flat "_")
        (whoami-db/select-whoami db)
-       (map (partial flat/flat->tree #"\$"))
-       (map coerce-whoami)
-       first))
+       first
+       (flat/flat->tree #"\$")))
+
+(defn find-whoami [db opts]
+  (-> (find-whoami-with-api-key-hash db opts)
+      (st/select-schema whoami-schema/Whoami)))
+
+(defn find-whoami-with-api-key [db opts api-key]
+  (let [whoami (find-whoami-with-api-key-hash db opts)]
+    (when (verified-api-key? api-key (:api-key-hash whoami))
+      (st/select-schema whoami whoami-schema/Whoami))))
 
 (defn update-kayttaja-with-whoami! [db whoami]
   (whoami-db/update-kayttaja-with-whoami! db (flat/tree->flat "_" whoami)))
