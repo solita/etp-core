5abe9fa2d4ddfe5c54d654cad3f89edba40586e4
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-10-11 21:42:24 +0300

    AE-395 - support for environments

--- etp-backend/src/test/resources/external-api/post.sh
+++ etp-backend/src/test/resources/external-api/post.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-file=${1-'complete-2018.json'}
-server=${2-'localhost:8080'}
+local='http://localhost:8080'
+dev='https://private.kehitys.energiatodistuspalvelu.com'
+test='https://private.test.energiatodistuspalvelu.com'
+
+server=${!1}
+file=${2-'complete-2018.json'}
 user=${3-'laatija1@example.com:asdfasdf'}
 
 curl -v -d "@$file" \
   -H "Content-Type: application/json" \
   -u $user \
-  "http://$server/api/external/energiatodistukset/2018"
+  "$server/api/external/energiatodistukset/2018"
