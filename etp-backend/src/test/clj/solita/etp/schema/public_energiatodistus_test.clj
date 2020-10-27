fb2e1ebd30dcd1e7a03250a850cabe8908afae15
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-10-27 16:41:19 +0200

    AE-676 add test for public energiatodistus schema

---
+++ etp-backend/src/test/clj/solita/etp/schema/public_energiatodistus_test.clj
@@ -0,0 +1,72 @@
+(ns solita.etp.schema.public-energiatodistus-test
+  (:require [clojure.test :as t]
+            [schema.core :as schema]
+            [solita.etp.schema.public-energiatodistus
+             :as public-energiatodistus-schema]))
+
+(def example-2013
+  {:laatija-fullname "Liisa Laatija"
+   :laatija-id 0
+   :voimassaolo-paattymisaika (java.time.Instant/now)
+   :tulokset {:e-luku 10
+              :e-luokka "B"
+              :kaytettavat-energiamuodot
+              {:fossiilinen-polttoaine 25000
+               :sahko 0
+               :kaukojaahdytys nil
+               :kaukolampo 25000
+               :uusiutuva-polttoaine 5000}}
+   :tila-id 2
+   :perustiedot {:havainnointikaynti (java.time.LocalDate/now)
+                 :rakennustunnus "1035150826"
+                 :katuosoite-sv nil
+                 :keskeiset-suositukset-fi "Suositukset FI"
+                 :kieli 0
+                 :nimi "Rakennuksen nimi"
+                 :postinumero "33100"
+                 :yritys {:nimi "Joku yritys Oy"
+                          :katuosoite "Hämeenkatu 1"
+                          :postinumero "33100"
+                          :postitoimipaikka "Tampere"}
+                 :kayttotarkoitus "RT"
+                 :katuosoite-fi "Hämeenkatu 10"
+                 :keskeiset-suositukset-sv "Suositukset SV"
+                 :valmistumisvuosi 2000}
+   :lahtotiedot {:lammitetty-nettoala 1500
+                 :ilmanvaihto {:tyyppi-id 4
+                               :kuvaus-fi "Ilmanvaihdon kuvaus FI"
+                               :kuvaus-sv "Ilmanvaihdon kuvaus SV"}
+                 :lammitys {:lammitysmuoto-1 {:id 0
+                                              :kuvaus-fi "Lämmitys 1 FI"
+                                              :kuvaus-sv "Lämmitys 1 SV"}
+                            :lammitysmuoto-2 {:id 6
+                                              :kuvaus-fi "Lämmitys 2 FI"
+                                              :kuvaus-sv "Lämmitys 2 SV"}
+                            :lammonjako {:id 10
+                                         :kuvaus-fi nil
+                                         :kuvaus-sv nil}}}
+   :id 200000
+   :versio 2013})
+
+(def example-2018
+  (-> example-2013
+      (assoc-in [:perustiedot :laatimisvaihe] 1)
+      (assoc :versio 2018)))
+
+(t/deftest PublicEnergiatodistus-test
+  (t/is (= example-2013
+           (schema/validate
+            public-energiatodistus-schema/PublicEnergiatodistus2013
+            example-2013)))
+  (t/is (= example-2013
+           (schema/validate
+            public-energiatodistus-schema/PublicEnergiatodistus
+            example-2013)))
+  (t/is (= example-2018
+           (schema/validate
+            public-energiatodistus-schema/PublicEnergiatodistus2018
+            example-2018)))
+  (t/is (= example-2018
+           (schema/validate
+            public-energiatodistus-schema/PublicEnergiatodistus
+            example-2018))))
