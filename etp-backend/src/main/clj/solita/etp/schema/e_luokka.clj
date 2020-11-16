d24f4be41b7920a745b70e2fbc4451365581a336
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-11-16 17:28:43 +0200

    AE-681 - fix schema

--- etp-backend/src/main/clj/solita/etp/schema/e_luokka.clj
+++ etp-backend/src/main/clj/solita/etp/schema/e_luokka.clj
@@ -3,6 +3,6 @@
             [solita.etp.schema.common :as common-schema]))
 
 (def ELuokka {:e-luokka schema/Str
-              :luokittelu common-schema/Luokittelu
+              :kayttotarkoitus common-schema/Luokittelu
               :raja-asteikko [[(schema/one schema/Int "raja") (schema/one schema/Str "luokka")]]
               (schema/optional-key :raja-uusi-2018) schema/Int})
