6971e2d7d075b3f4c87c061686310e7fa3988776
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-12-15 16:24:11 +0200

    AE-892 update select-liite sql to use information from audit schema

--- etp-backend/src/main/sql/solita/etp/db/liite.sql
+++ etp-backend/src/main/sql/solita/etp/db/liite.sql
@@ -8,11 +8,13 @@ select liite.nimi, liite.contenttype from liite
 where id = :id
 
 -- name: select-liite-by-energiatodistus-id
-select liite.id, liite.createtime, fullname(kayttaja.*) "author-fullname",
-  liite.nimi, liite.contenttype, liite.url
-from liite inner join kayttaja on kayttaja.id = liite.createdby_id
-where energiatodistus_id = :energiatodistus-id
-order by liite.createtime desc
+select distinct on (a.id) l.id, a.modifytime createtime,
+  fullname(k.*) "author-fullname", l.nimi, l.contenttype, l.url
+from liite l
+inner join audit.liite a on l.id = a.id
+inner join kayttaja k on a.modifiedby_id = k.id
+where l.energiatodistus_id = :energiatodistus-id
+order by a.id desc
 
 -- name: delete-liite!
 delete from liite where id = :id
