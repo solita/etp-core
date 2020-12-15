9e36c967aa9a91d6a658c3be732b3b4db20a565d
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-12-15 20:33:09 +0200

    AE-892 fix select liite sql

--- etp-backend/src/main/sql/solita/etp/db/liite.sql
+++ etp-backend/src/main/sql/solita/etp/db/liite.sql
@@ -8,13 +8,13 @@ select liite.nimi, liite.contenttype from liite
 where id = :id
 
 -- name: select-liite-by-energiatodistus-id
-select distinct on (a.id) l.id, a.modifytime createtime,
+select distinct on (l.id) l.id, a.modifytime createtime,
   fullname(k.*) "author-fullname", l.nimi, l.contenttype, l.url
 from liite l
 inner join audit.liite a on l.id = a.id
 inner join kayttaja k on a.modifiedby_id = k.id
 where l.energiatodistus_id = :energiatodistus-id
-order by a.id desc
+order by l.id, a.modifytime asc, a.event_id desc
 
 -- name: delete-liite!
 delete from liite where id = :id
