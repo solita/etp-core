225f5e5434a7c2e54108586ab0e2a78731781bff
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-12-17 15:34:54 +0200

    AE-738 - use common audit tools for energiatodistus_tila audit

--- etp-backend/src/main/sql/solita/etp/db/energiatodistus.sql
+++ etp-backend/src/main/sql/solita/etp/db/energiatodistus.sql
@@ -42,9 +42,9 @@ where tila_id in (et_tilat.allekirjoitettu, et_tilat.hylatty) and id = :id
 -- name: revert-energiatodistus-korvattu!
 update energiatodistus set
   tila_id = (
-    select history.tila_id from energiatodistus_tila_history history
-    where history.energiatodistus_id = energiatodistus.id
-    order by history.modifytime desc, history.id desc limit 1 offset 1)
+    select history.tila_id from audit.energiatodistus_tila history
+    where history.id = energiatodistus.id
+    order by history.modifytime desc, history.event_id desc limit 1 offset 1)
 from et_tilat
 where tila_id = et_tilat.korvattu and id = :id
 
--- etp-db/src/main/sql/migration/afterMigrate.sql
+++ etp-db/src/main/sql/migration/afterMigrate.sql
@@ -6,9 +6,7 @@ grant usage on schema audit to etp_app;
 
 grant insert, update on table kayttaja to etp_app;
 grant insert, update on table laatija to etp_app;
-grant insert, update, delete on table energiatodistus to etp_app;
+grant insert, update on table energiatodistus to etp_app;
 grant insert, update on table yritys to etp_app;
 grant insert, update on table laatija_yritys to etp_app;
-grant insert, update, delete on table liite to etp_app;
-
-grant insert on table energiatodistus_tila_history to etp_app;
+grant insert, update on table liite to etp_app;
