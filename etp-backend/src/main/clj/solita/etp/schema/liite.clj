2d1175a7b294496f4a676c359e8f9ce5b36fc1a2
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-12-22 13:44:30 +0200

    AE-793 add url validation to link attachments

--- etp-backend/src/main/clj/solita/etp/schema/liite.clj
+++ etp-backend/src/main/clj/solita/etp/schema/liite.clj
@@ -2,13 +2,19 @@
   (:require [schema.core :as schema]
             [solita.etp.schema.common :as common-schema]))
 
+(defn valid-attachment-url? [s]
+  (re-matches #"^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*$)"
+              s))
+
+(def Url (schema/constrained schema/Str valid-attachment-url?))
 
 (def Liite {:id              common-schema/Key
             :createtime      common-schema/Instant
             :author-fullname schema/Str
             :nimi            schema/Str
             :contenttype     (schema/maybe schema/Str)
-            :url             (schema/maybe schema/Str)})
+            :url             (schema/maybe Url)})
 
-(def LiiteLinkAdd {:nimi schema/Str, :url schema/Str})
+(def LiiteLinkAdd {:nimi schema/Str
+                   :url Url})
 
