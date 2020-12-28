6915afaf75d6bd4bfb6fd7335e95bfd11ea8e086
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2020-12-28 18:33:24 +0200

    AE-902 - kirjautumattomia (login = null) laatijoita ei näytetä julkisella puolella - only login + cognitoid is updated in whoami

--- etp-backend/src/main/clj/solita/etp/service/laatija.clj
+++ etp-backend/src/main/clj/solita/etp/service/laatija.clj
@@ -13,10 +13,10 @@
 ;; *** Require sql functions ***
 (db/require-queries 'laatija)
 
-(defn public-laatija [{:keys [voimassa laatimiskielto julkinenpuhelin
+(defn public-laatija [{:keys [login voimassa laatimiskielto julkinenpuhelin
                               julkinenemail julkinenwwwosoite julkinenosoite]
                        :as laatija}]
-  (when (and voimassa (not laatimiskielto))
+  (when (and voimassa (not laatimiskielto) (not (nil? login)))
     (select-keys laatija
                  (cond-> laatija-schema/always-public-kayttaja-laatija-ks
                    julkinenpuhelin (conj :puhelin)
