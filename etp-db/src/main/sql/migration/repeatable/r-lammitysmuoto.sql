8d9e3033d823595d6f778a4515eb276167dee5a2
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-12-29 13:59:47 +0200

    AE-936 add swedish translations to some luokittelut

--- etp-db/src/main/sql/migration/repeatable/r-lammitysmuoto.sql
+++ etp-db/src/main/sql/migration/repeatable/r-lammitysmuoto.sql
@@ -1,15 +1,15 @@
 INSERT INTO lammitysmuoto (id, label_fi, label_sv, ordinal)
 VALUES
-(0, 'Kaukolämpö', '', 1),
-(1, 'Sähkö', '', 2),
-(2, 'Puu', '', 3),
-(3, 'Pelletti', '', 4),
-(4, 'Öljy', '', 5),
-(5, 'Kaasu', '', 6),
-(6, 'MILP', '', 7),
-(7, 'VILP / IILP', '', 8),
-(8, 'PILP', '', 9),
-(9, 'Muu, mikä', '', 10)
+(0, 'Kaukolämpö', 'Fjärrvärme', 1),
+(1, 'Sähkö', 'El', 2),
+(2, 'Puu', 'Trä', 3),
+(3, 'Pelletti', 'Pellet', 4),
+(4, 'Öljy', 'Olja', 5),
+(5, 'Kaasu', 'Gas', 6),
+(6, 'Maalämpöpumppu', 'Jordvärmepump', 7),
+(7, 'Vesi-ilmalämpöpumppu', 'Vatten-luftvärmepump', 8),
+(8, 'Poistoilmalämpöpumppu', 'Frånluftsvärmepump', 9),
+(9, 'Muu, mikä', 'Annat, vad?', 10)
 ON CONFLICT (id) DO UPDATE
 SET
 label_fi = excluded.label_fi,
