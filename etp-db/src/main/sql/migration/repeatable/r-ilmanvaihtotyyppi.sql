2b163c8c62f2cff48079c33e615d7ea018e0f1d6
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2020-12-31 11:04:09 +0200

    AE-968 change order of new ilmanvaihtotyypit

--- etp-db/src/main/sql/migration/repeatable/r-ilmanvaihtotyyppi.sql
+++ etp-db/src/main/sql/migration/repeatable/r-ilmanvaihtotyyppi.sql
@@ -2,13 +2,13 @@ INSERT INTO ilmanvaihtotyyppi (id, label_fi, label_sv, ordinal)
 VALUES
 (0, 'Painovoimainen ilmanvaihtojärjestelmä', 'Självdragsventilationssystem', 1),
 (1, 'Koneellinen poistoilmanvaihtojärjestelmä', 'Maskinellt frånluftsventilationssystem', 2),
-(2, 'Koneellinen tulo- ja poistoilmanvaihtojärjestelmä', 'Maskinellt till- och frånluftsventilationssystem', 3),
-(3, 'Poistoilmalämpöpumppu (vain käyttötarkoitusluokka 1)', 'Frånluftsvärmepump (endast användningsklass 1)', 4),
-(4, 'Painovoimainen/koneellinen poisto', 'Naturlig/maskinell frånluftsventilation', 5),
-(5, 'Painovoimainen/koneellinen tulo- ja poisto', 'Naturlig/maskinell till- och frånluftsventilation', 6),
+(2, 'Koneellinen tulo- ja poistoilmanvaihtojärjestelmä', 'Maskinellt till- och frånluftsventilationssystem', 4),
+(3, 'Poistoilmalämpöpumppu (vain käyttötarkoitusluokka 1)', 'Frånluftsvärmepump (endast användningsklass 1)', 6),
+(4, 'Painovoimainen/koneellinen poisto', 'Naturlig/maskinell frånluftsventilation', 7),
+(5, 'Painovoimainen/koneellinen tulo- ja poisto', 'Naturlig/maskinell till- och frånluftsventilation', 8),
 (6, 'Muu, mikä', 'Annat, vad?', 9),
-(7, 'Koneellinen poistoilmanvaihtojärjestelmä lämmöntalteenotolla', 'Mekaniskt frånluftsventilationssystem med värmeåtervinning', 7),
-(8, 'Koneellinen tulo- ja poistoilmanvaihtojärjestelmä lämmöntalteenotolla', 'Mekaniskt till- och frånluftsventilationssystem med värmeåtervinning', 8)
+(7, 'Koneellinen poistoilmanvaihtojärjestelmä lämmöntalteenotolla', 'Mekaniskt frånluftsventilationssystem med värmeåtervinning', 3),
+(8, 'Koneellinen tulo- ja poistoilmanvaihtojärjestelmä lämmöntalteenotolla', 'Mekaniskt till- och frånluftsventilationssystem med värmeåtervinning', 5)
 
 ON CONFLICT (id) DO UPDATE
 SET
