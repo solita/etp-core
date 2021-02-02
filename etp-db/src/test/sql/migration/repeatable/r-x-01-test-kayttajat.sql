742ca20a2b73a090e3968a3d0832ce7d5d834a3c
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2021-02-02 09:33:11 +0200

    AE-1060: update laatija along with kayttaja

--- etp-db/src/test/sql/migration/repeatable/r-x-01-test-kayttajat.sql
+++ etp-db/src/test/sql/migration/repeatable/r-x-01-test-kayttajat.sql
@@ -32,3 +32,16 @@ on conflict (email) do update
       virtu$localid  = excluded.virtu$localid,
       virtu$organisaatio = excluded.virtu$organisaatio;
 
+INSERT INTO laatija (id, patevyystaso, toteamispaivamaara, toteaja, jakeluosoite, postinumero, postitoimipaikka)
+SELECT id, 2, date '2020-01-01', 'KIINKO', 'Peltokatu 26', '33100', 'Tampere' FROM kayttaja WHERE email ILIKE '%solita.fi%' AND rooli_id = 0
+UNION ALL
+SELECT id, 2, date '2020-01-01', 'FISE', 'Vesijärvenkatu 11 A', '15140', 'Lahti' FROM kayttaja WHERE email ILIKE '%ara.fi%' AND rooli_id = 0
+UNION ALL
+SELECT id, 1, date '2020-10-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email NOT ILIKE '%solita.fi%' AND email NOT ILIKE '%ara.fi%' AND rooli_id = 0
+on conflict (id) do update
+  set patevyystaso = excluded.patevyystaso,
+      toteamispaivamaara = excluded.toteamispaivamaara,
+      toteaja = excluded.toteaja,
+      jakeluosoite = excluded.jakeluosoite,
+      postinumero  = excluded.postinumero,
+      postitoimipaikka = excluded.postitoimipaikka;
