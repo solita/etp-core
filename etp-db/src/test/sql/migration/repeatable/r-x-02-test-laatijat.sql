95ffb19a65b00ee0d39bf6e948073a092e18bf08
Toni Pulkkinen <60097337+solita-tonipu@users.noreply.github.com>
2021-02-02 18:16:09 +0200

    AE-1060: update laatija along with kayttaja

--- etp-db/src/test/sql/migration/repeatable/r-x-02-test-laatijat.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO laatija (id, patevyystaso, toteamispaivamaara, toteaja, jakeluosoite, postinumero, postitoimipaikka)
-SELECT id, 2, date '2020-01-01', 'KIINKO', 'Peltokatu 26', '33100', 'Tampere' FROM kayttaja WHERE email ILIKE '%solita.fi%' AND rooli_id = 0
-UNION ALL
-SELECT id, 2, date '2020-01-01', 'FISE', 'Vesijärvenkatu 11 A', '15140', 'Lahti' FROM kayttaja WHERE email ILIKE '%ara.fi%' AND rooli_id = 0
-UNION ALL
-SELECT id, 1, date '2020-10-01', 'FISE', 'Hämeenkatu 1', '33100', 'Tampere' FROM kayttaja WHERE email NOT ILIKE '%solita.fi%' AND email NOT ILIKE '%ara.fi%' AND rooli_id = 0;
