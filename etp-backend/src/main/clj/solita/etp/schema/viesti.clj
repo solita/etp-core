1ea18c176b5aaa1d86dcd54ea8e261d00005f691
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-02-04 15:11:07 +0200

    AE-1069 - viesti api: include more information about the message sender (from) - remove redundant from-id from ketju - the sender of ketju is the sender of first message

--- etp-backend/src/main/clj/solita/etp/schema/viesti.clj
+++ etp-backend/src/main/clj/solita/etp/schema/viesti.clj
@@ -11,14 +11,19 @@
    :subject            schema/Str
    :body               schema/Str})
 
+(def Sender
+  {:id       common-schema/Key
+   :rooli-id common-schema/Key
+   :sukunimi schema/Str
+   :etunimi  schema/Str})
+
 (def Viesti
-  {:from-id  common-schema/Key
+  {:from     Sender
    :senttime common-schema/Instant
    :body     schema/Str})
 
 (def Ketju
   {:id                 common-schema/Key
-   :from-id            common-schema/Key
    :kayttajat          [common-schema/Key]
    :kayttajarooli-id   (schema/maybe common-schema/Key)
    :kayttajaryhma-id   (schema/maybe common-schema/Key)
