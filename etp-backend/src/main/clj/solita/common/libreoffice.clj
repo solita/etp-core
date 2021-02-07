2865a76d824e860520571c8bd94cb74cd4c9dc01
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-02-07 12:40:07 +0200

    Same libre configuration (./libreoffice) for all environments. Project files in docker image is in folder /etp. Note: start.sh should work inside docker and locally when uberjar is built.

--- etp-backend/src/main/clj/solita/common/libreoffice.clj
+++ etp-backend/src/main/clj/solita/common/libreoffice.clj
@@ -1,39 +1,41 @@
 (ns solita.common.libreoffice
   (:require [clojure.java.io :as io]
             [clojure.java.shell :as shell])
-  (:import (java.io IOException)
-           (java.nio.file Files)
+  (:import (java.io IOException File)
+           (java.nio.file Files Path)
            (java.nio.file.attribute FileAttribute)))
 
 ;; Recursive delete from https://gist.github.com/olieidel/c551a911a4798312e4ef42a584677397
-(defn rm [file]
+(defn rm [^File file]
   (when (.isDirectory file)
     (doseq [file-in-dir (.listFiles file)]
       (rm file-in-dir)))
   (io/delete-file file))
 
-(defn rm-path [path]
+(defn rm-path [^Path path]
   (let [file (.toFile path)]
-    (if (.exists file)
-      (rm (.toFile path)))))
+    (when (.exists file) (rm file))))
 
-(defn config-path []
-  ;; The configuration can likely be overridden to be somewhere else,
-  ;; but this should be a reasonably safe assumption.
-  (str (System/getenv "HOME") "/.config/libreoffice/4"))
+(def config-path
+  ;; The libreoffice configuration is defined in this directory.
+  ;; Directory must exists but can be empty if no specific configuration is required.
+  "./libreoffice")
 
 (defn make-tmpdir []
   (let [attrs (make-array FileAttribute 0)]
     (Files/createTempDirectory "libreoffice-user" attrs)))
 
 (defn populate-tmpdir [path]
-  (let [src-dir (config-path)
-        cmd ["cp" "-r" src-dir (str path "/config")]
+  (let [target (str path "/config")
+        cmd ["cp" "-r" config-path target]
         result (apply shell/sh cmd)]
     (when (not (= 0 (:exit result)))
-      (throw (IOException. (:err result))))))
+      (throw (IOException.
+               (str "Libreoffice configuration copy from: " config-path
+                    " to: " target " failed for: "
+                    (:err result)))))))
 
-(defn exec-lo [tmpdir args]
+(defn exec-libreoffice [tmpdir args]
   (let [dirparam (str "-env:UserInstallation=file://" tmpdir "/config")]
     (apply shell/sh
            "libreoffice"
@@ -45,6 +47,6 @@
   (let [tmpdir (make-tmpdir)]
     (try
       (populate-tmpdir tmpdir)
-      (exec-lo tmpdir args)
+      (exec-libreoffice tmpdir args)
       (finally (rm-path tmpdir)))))
 
