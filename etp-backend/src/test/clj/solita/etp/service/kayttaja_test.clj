49986e8e7ba0cdd40b878dd25423c8bf08ad7b45
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2021-02-19 10:41:19 +0200

    AE-373 refactor kayttaja service tests to use new test data

--- etp-backend/src/test/clj/solita/etp/service/kayttaja_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/kayttaja_test.clj
@@ -1,10 +1,9 @@
 (ns solita.etp.service.kayttaja-test
   (:require [clojure.test :as t]
             [schema.core :as schema]
-            [schema-generators.generators :as g]
             [solita.common.map :as map]
             [solita.etp.test-system :as ts]
-            [solita.etp.test-utils :as tu]
+            [solita.etp.test-data.kayttaja :as kayttaja-test-data]
             [solita.etp.service.kayttaja :as service]
             [solita.etp.service.rooli :as rooli-service]
             [solita.etp.schema.kayttaja :as kayttaja-schema]
@@ -13,66 +12,59 @@
 
 (t/use-fixtures :each ts/fixture)
 
-(def laatija {:rooli 0})
-(def patevyydentoteaja {:rooli 1})
-(def paakayttaja {:rooli 2})
-(def roolit [laatija patevyydentoteaja paakayttaja])
-
-(defn find-kayttaja [whoami id]
-  (try
-    (service/find-kayttaja ts/*db* whoami id)
-    (catch Exception e (if (not= (-> e ex-data :type) :forbidden)
-                         (throw e)))))
+(defn test-data-set []
+  {:kayttajat (kayttaja-test-data/generate-and-insert! 200)})
 
 (t/deftest add-and-find-test
-  (doseq [kayttaja (tu/generate-kayttaja 100 laatija-schema/KayttajaAdd)
-          :let [id (service/add-kayttaja! ts/*db* kayttaja)
-                whoami (rand-nth (conj roolit {:id id}))
-                found (find-kayttaja whoami id)]]
-    (if (= whoami laatija)
-      (t/is (nil? found))
-      (do
+  (let [{:keys [kayttajat]} (test-data-set)]
+    (doseq [[id kayttaja] kayttajat
+            :let [whoami (rand-nth [kayttaja-test-data/paakayttaja
+                                    kayttaja-test-data/laskuttaja
+                                    {:id id}])
+                  found (service/find-kayttaja ts/*db* whoami id)]]
+      (schema/validate kayttaja-schema/Kayttaja found)
+      (t/is (map/submap? kayttaja found)))))
+
+(t/deftest add-and-find-no-permissions-test
+  (let [{:keys [kayttajat]} (test-data-set)]
+    (doseq [[id _] kayttajat]
+      (t/is (thrown-with-msg?
+             clojure.lang.ExceptionInfo
+             #"Forbidden"
+             (service/find-kayttaja ts/*db*
+                                    (rand-nth [kayttaja-test-data/laatija
+                                               kayttaja-test-data/patevyyden-toteaja])
+                                    id))))))
+
+(t/deftest update-and-find-test
+  (let [{:keys [kayttajat]} (test-data-set)
+        updates (kayttaja-test-data/generate-updates (count kayttajat))]
+    (doseq [[[id kayttaja] update] (->> (interleave kayttajat updates)
+                                        (partition 2))]
+      (service/update-kayttaja! ts/*db*
+                                kayttaja-test-data/paakayttaja
+                                id
+                                update)
+      (let [found (service/find-kayttaja
+                   ts/*db*
+                   (rand-nth [kayttaja-test-data/paakayttaja
+                              kayttaja-test-data/laskuttaja])
+                   id)]
         (schema/validate kayttaja-schema/Kayttaja found)
-        (t/is (map/submap? kayttaja found))))))
+        (t/is (map/submap? update found))))))
 
-(defn assoc-idx-email [idx kayttaja]
-  (assoc kayttaja :email (str "kayttaja" idx "@example.com")))
-
-(defn update-kayttaja! [whoami id kayttaja]
-  (try
-    (service/update-kayttaja! ts/*db* whoami id kayttaja)
-    (catch Exception e (if (not= (-> e ex-data :type) :forbidden)
-                         (throw e)))))
-
-(t/deftest add-update-and-find-test
-  (doseq [[new update] (->>
-                        (interleave
-                         (tu/generate-kayttaja 100 kayttaja-schema/KayttajaAdd)
-                         (tu/generate-kayttaja 100 kayttaja-schema/KayttajaUpdate))
-                        (partition-all 2))
-          :let [id (service/add-kayttaja! ts/*db* new)
-                whoami (rand-nth (conj roolit {:id id}))
-                _ (update-kayttaja! whoami id update)
-                new-rooli (:rooli update)
-                found (find-kayttaja whoami id)]]
-    (cond
-      ;; If whoami has no permission to find the käyttäjä
-      (not (or (= (:id whoami) id)
-               (= whoami paakayttaja)
-               (and (= whoami patevyydentoteaja)
-                    (rooli-service/laatija? found))))
-      (t/is (nil? found))
-
-      ;; If whoami has no permission to update the käyttäjä
-      (not (or (and (= (:id whoami) id)
-                    (common-schema/not-contains-keys
-                      update
-                      kayttaja-schema/KayttajaAdminUpdate))
-               (= whoami paakayttaja)))
-      (do (schema/validate kayttaja-schema/Kayttaja found)
-          (t/is (map/submap? new found)))
-
-      ;; Otherwise the update was a success
-      :else
-      (do (schema/validate kayttaja-schema/Kayttaja found)
-          (t/is (map/submap? update found))))))
+(t/deftest update-and-find-no-permissions-test
+  (let [{:keys [kayttajat]} (test-data-set)
+        updates (kayttaja-test-data/generate-updates (count kayttajat))]
+    (doseq [[[id kayttaja] update] (->> (interleave kayttajat updates)
+                                        (partition 2))]
+      (t/is (thrown-with-msg?
+             clojure.lang.ExceptionInfo
+             #"Forbidden"
+             (service/update-kayttaja!
+              ts/*db*
+              (rand-nth [kayttaja-test-data/laatija
+                         kayttaja-test-data/patevyyden-toteaja
+                         kayttaja-test-data/laskuttaja])
+              id
+              update))))))
--- etp-backend/src/test/clj/solita/etp/test_data/kayttaja.clj
+++ etp-backend/src/test/clj/solita/etp/test_data/kayttaja.clj
@@ -1,5 +1,30 @@
-(ns solita.etp.test-data.kayttaja)
+(ns solita.etp.test-data.kayttaja
+  (:require [solita.etp.test-system :as ts]
+            [solita.etp.test-data.generators :as generators]
+            [solita.etp.schema.kayttaja :as kayttaja-schema]
+            [solita.etp.service.kayttaja :as kayttaja-service]))
 
+(def laatija {:rooli 0})
 (def patevyyden-toteaja {:rooli 1})
 (def paakayttaja {:rooli 2})
+(def laskuttaja {:rooli 3})
 (def public nil)
+
+(defn generate-adds [n]
+  (take n (map #(generators/complete {:email %}
+                                     kayttaja-schema/KayttajaAdd)
+               (generators/unique-emails n))))
+
+(defn generate-updates [n]
+  (take n (map #(generators/complete {:henkilotunnus %1
+                                      :email %2}
+                                     kayttaja-schema/KayttajaUpdate)
+               (generators/unique-henkilotunnukset n)
+               (generators/unique-emails n))))
+
+(defn insert! [kayttaja-adds]
+  (doall (map #(kayttaja-service/add-kayttaja! ts/*db* %) kayttaja-adds)))
+
+(defn generate-and-insert! [n]
+  (let [kayttaja-adds (generate-adds n)]
+    (zipmap (insert! kayttaja-adds) kayttaja-adds)))
