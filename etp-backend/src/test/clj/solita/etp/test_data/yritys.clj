c9104798039acbf80d49bf3959a75b4da4a0df5b
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2021-03-02 11:57:30 +0200

    AE-373 refactor yritys tests data

--- etp-backend/src/test/clj/solita/etp/test_data/yritys.clj
+++ etp-backend/src/test/clj/solita/etp/test_data/yritys.clj
@@ -4,17 +4,18 @@
             [solita.etp.schema.yritys :as yritys-schema]
             [solita.etp.service.yritys :as yritys-service]))
 
-(defn generate [n]
+(defn generate-adds [n]
   (take n (map #(generators/complete {:ytunnus %
                                       :verkkolaskuoperaattori (rand-int 32)}
                                      yritys-schema/YritysSave)
                generators/unique-ytunnukset)))
 
-(defn insert! [whoami yritykset]
-  (mapv #(yritys-service/add-yritys! (ts/db-user (:id whoami))
-                                     whoami
-                                     %)
-        yritykset))
+(def generate-updates generate-adds)
 
-(defn generate-and-insert! [whoami n]
-  (insert! (generate n) whoami))
+(defn insert! [yritys-adds laatija-id]
+  (mapv #(yritys-service/add-yritys! (ts/db-user laatija-id) {:id laatija-id} %)
+        yritys-adds))
+
+(defn generate-and-insert! [n laatija-id]
+  (let [yritys-adds (generate-adds n)]
+    (zipmap (insert! yritys-adds laatija-id) yritys-adds)))
