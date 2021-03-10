1ace4b4b52306e1f9f9a9c72cb1df9eb683eb82e
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2021-03-10 08:43:22 +0200

    AE-372 add rooli tests for laskuttaja

--- etp-backend/src/test/clj/solita/etp/service/rooli_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/rooli_test.clj
@@ -20,22 +20,26 @@
   (t/is (false? (service/laatija? nil?)))
   (t/is (true? (service/laatija? {:rooli 0})))
   (t/is (false? (service/laatija? {:rooli 1})))
-  (t/is (false? (service/laatija? {:rooli 2}))))
+  (t/is (false? (service/laatija? {:rooli 2})))
+  (t/is (false? (service/laatija? {:rooli 3}))))
 
 (t/deftest patevyydentoteaja?-test
   (t/is (false? (service/patevyydentoteaja? nil?)))
   (t/is (false? (service/patevyydentoteaja? {:rooli 0})))
   (t/is (true? (service/patevyydentoteaja? {:rooli 1})))
-  (t/is (false? (service/patevyydentoteaja? {:rooli 2}))))
+  (t/is (false? (service/patevyydentoteaja? {:rooli 2})))
+  (t/is (false? (service/paakayttaja? {:rooli 3}))))
 
 (t/deftest paakayttaja?-test
   (t/is (false? (service/paakayttaja? nil?)))
   (t/is (false? (service/paakayttaja? {:rooli 0})))
   (t/is (false? (service/paakayttaja? {:rooli 1})))
-  (t/is (true? (service/paakayttaja? {:rooli 2}))))
+  (t/is (true? (service/paakayttaja? {:rooli 2})))
+  (t/is (false? (service/paakayttaja? {:rooli 3}))))
 
-(t/deftest laatija-maintainer?-test
-  (t/is (false? (service/laatija-maintainer? nil?)))
-  (t/is (false? (service/laatija-maintainer? {:rooli 0})))
-  (t/is (true? (service/laatija-maintainer? {:rooli 1})))
-  (t/is (true? (service/laatija-maintainer? {:rooli 2}))))
+(t/deftest laskuttaja?-test
+  (t/is (false? (service/laskuttaja? nil?)))
+  (t/is (false? (service/laskuttaja? {:rooli 0})))
+  (t/is (false? (service/laskuttaja? {:rooli 1})))
+  (t/is (false? (service/laskuttaja? {:rooli 2})))
+  (t/is (true? (service/laskuttaja? {:rooli 3}))))
