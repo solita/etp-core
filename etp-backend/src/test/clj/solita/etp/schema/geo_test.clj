5f97c8963901ce5d335789155135421bf5cc1c85
Toni Pulkkinen <60097337+solita-tonipu@users.noreply.github.com>
2021-03-12 11:49:28 +0200

    AE-1135: fixed postinumero schema validation

---
+++ etp-backend/src/test/clj/solita/etp/schema/geo_test.clj
@@ -0,0 +1,12 @@
+(ns solita.etp.schema.geo-test
+  (:require [clojure.test :as t]
+            [schema.core :as schema]
+            [solita.etp.schema.geo :as geo]))
+
+(t/deftest valid-postinumero?-test
+  (t/is (some? (schema/check geo/PostinumeroFI nil)))
+  (t/is (some? (schema/check geo/PostinumeroFI 00100)))
+  (t/is (some? (schema/check geo/PostinumeroFI "00100, Helsinki")))
+  (t/is (some? (schema/check geo/PostinumeroFI "0")))
+
+  (t/is (nil? (schema/check geo/PostinumeroFI "00100"))))
