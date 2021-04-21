34778f0c2f940a0eed8117dee87acf5e63a2199c
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2021-04-21 13:30:53 +0300

    log toimenpide

--- etp-backend/src/main/clj/solita/etp/service/asha_valvonta_oikeellisuus.clj
+++ etp-backend/src/main/clj/solita/etp/service/asha_valvonta_oikeellisuus.clj
@@ -2,28 +2,58 @@
   (:require [solita.etp.service.asha :as asha]
             [solita.etp.service.energiatodistus :as energiatodistus-service]
             [solita.etp.service.kayttaja :as kayttaja-service]
-            [clojure.string :as str]))
+            [flathead.deep :as deep]
+            [clojure.string :as str]
+            [solita.etp.exception :as exception]))
+
+(defn resolve-energiatodistus-laatija [db toimenpide]
+  (let [energiatodistus (energiatodistus-service/find-energiatodistus-any-laatija db (:energiatodistus-id toimenpide))
+        laatija (kayttaja-service/find-kayttaja db (:laatija-id energiatodistus))]
+    (if (and energiatodistus laatija)
+      {:energiatodistus energiatodistus
+       :laatija         laatija}
+      (exception/throw-ex-info!
+        :failed-to-resolve-energiatodistus-or-laatija-from-toimenpide
+        "Failed to resolve energiatodistus or laatija from toimenpide"))))
+
+(defn request-id [energiatodistus toimenpide]
+  (str (:id energiatodistus) "/" (:id toimenpide)))
 
 (defn create-case [whoami db toimenpide]
-  (when-let [energiatodistus (energiatodistus-service/find-energiatodistus-any-laatija db (:energiatodistus-id toimenpide))]
-    (let [laatija (kayttaja-service/find-kayttaja db (:laatija-id energiatodistus))]
-      (asha/case-create {:request-id     (:email whoami)
-                         :sender-id      (str (:id energiatodistus) "/" (:id toimenpide))
-                         :classification "05.03.02"
-                         :service        "general"          ; Yleinen menettely
-                         :name           (str/join ", " [(-> energiatodistus :perustiedot :katuosoite-fi) (:laatija-fullname energiatodistus)])
-                         :description    (-> energiatodistus :perustiedot :rakennustunnus)
-                         :attach         {:contact (asha/kayttaja->contact laatija)}}))))
+  (let [{:keys [energiatodistus laatija]} (resolve-energiatodistus-laatija db toimenpide)]
+    (asha/case-create {:request-id     (request-id energiatodistus toimenpide)
+                       :sender-id      (:email whoami)
+                       :classification "05.03.02"
+                       :service        "general"            ; Yleinen menettely
+                       :name           (str/join ", " [(-> energiatodistus :perustiedot :katuosoite-fi) (:laatija-fullname energiatodistus)])
+                       :description    (-> energiatodistus :perustiedot :rakennustunnus)
+                       :attach         {:contact (asha/kayttaja->contact laatija)}})))
 
-(defn create-tietopyynto [sender-id request-id case-number users document]
-  (asha/execute-operation {:sender-id         sender-id
-                           :request-id        request-id
-                           :identity          {:case              {:number case-number}
-                                               :processing-action {:name-identity "Vireillepano"}}
-                           :processing-action {:name                 "Tietopyyntö"
-                                               :reception-date       (java.time.Instant/now)
-                                               :expected-end-date    (.plus (java.time.Instant/now) 30 java.time.temporal.ChronoUnit/DAYS)
-                                               :contacting-direction "SENT"
-                                               :contact              (map asha/kayttaja->contact users)}})
-  (asha/add-documents-to-processing-action sender-id request-id case-number "Tietopyyntö" [(assoc document :type "Pyyntö")])
-  (asha/take-processing-action sender-id request-id case-number "Tietopyyntö"))
+(defn- available-processing-actions [toimenpide laatija]
+  {:rfi-request {:identity          {:case              {:number (:diaarinumero toimenpide)}
+                                     :processing-action {:name-identity "Vireillepano"}}
+                 :processing-action {:name                 "Tietopyyntö"
+                                     :reception-date       (java.time.Instant/now)
+                                     :expected-end-date    (.plus (java.time.Instant/now) 30 java.time.temporal.ChronoUnit/DAYS)
+                                     :contacting-direction "SENT"
+                                     :contact              (asha/kayttaja->contact laatija)}
+                 :document          {:type "Päätös"}}})
+
+(defn log-tomenpide [whoami db toimenpide document]
+  (let [{:keys [energiatodistus laatija]} (resolve-energiatodistus-laatija db toimenpide)
+        processing-action (:rfi-request (available-processing-actions toimenpide laatija))
+        request-id (request-id energiatodistus toimenpide)
+        sender-id (:email whoami)
+        case-number (:diaarinumero toimenpide)]
+    (asha/execute-operation {:request-id        request-id
+                             :sender-id         sender-id
+                             :identity          (:identity processing-action)
+                             :processing-action (:processing-action processing-action)})
+    (when document
+      (asha/add-documents-to-processing-action
+        sender-id
+        request-id
+        case-number
+        (-> processing-action :processing-action :name)
+        [(assoc document :type (-> processing-action :document :type))]))
+    (asha/take-processing-action sender-id request-id case-number (-> processing-action :processing-action :name))))
