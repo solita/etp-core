65452cdba1238f6a43ffebcc37487f4038a5115d
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2021-05-20 09:47:24 +0300

    AE-1284 fix minus sign in csv export and improve related test

--- etp-backend/src/test/clj/solita/etp/service/energiatodistus_csv_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/energiatodistus_csv_test.clj
@@ -9,7 +9,8 @@
             [solita.etp.service.complete-energiatodistus
              :as complete-energiatodistus-service]
             [solita.etp.service.energiatodistus-csv :as service])
-  (:import (java.io ByteArrayOutputStream)))
+  (:import (java.io ByteArrayOutputStream)
+           (java.time Instant)))
 
 (t/use-fixtures :each ts/fixture)
 
@@ -57,8 +58,11 @@
 
 (t/deftest csv-line-test
   (t/is (= "\n" (service/csv-line [])))
-  (t/is (= "\"test\";1,235;2\n" (service/csv-line
-                             ["test" 1.23456 2]))))
+  (t/is (= "\"test\";1,235;-15;2021-01-01T14:15\n"
+           (service/csv-line ["test"
+                              1.23456
+                              -15
+                              (Instant/parse "2021-01-01T12:15:00.000Z")]))))
 
 (t/deftest write-energiatodistukset-csv-test
   (let [{:keys [laatijat energiatodistukset]} (test-data-set)
