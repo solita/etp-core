70229d2f34c6e531c25f2d717de53aa2c916117b
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2021-06-07 18:59:50 +0300

    AE-1306: fix template filename typo

--- etp-backend/src/main/clj/solita/etp/service/asha_valvonta_oikeellisuus.clj
+++ etp-backend/src/main/clj/solita/etp/service/asha_valvonta_oikeellisuus.clj
@@ -82,13 +82,13 @@
 
 (defn template-id->template [template-id]
   (let [file (case template-id
-               1 "pdf/taustamateriaali-toimityspyynto.html"
+               1 "pdf/taustamateriaali-toimituspyynto.html"
                2 "pdf/taustamateriaali-kehotus.html"
                3 "pdf/taustamateriaali-varoitus.html"
                4 "pdf/valvontamuistio.html"
                5 "pdf/valvontamuistio-kehotus.html"
                6 "pdf/valvontamuistio-varoitus.html"
-               "pdf/taustamateriaali-toimityspyynto.html")]
+               "pdf/taustamateriaali-toimituspyynto.html")]
     (-> file io/resource slurp)))
 
 (defn generate-template [db whoami toimenpide energiatodistus laatija]
