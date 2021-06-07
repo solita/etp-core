efdc403b9ee6b8a0a27552c27b6313ec412859e3
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2021-06-07 13:18:49 +0300

    AE-1340 implement CRUD for käytönvalvonta mock api

--- etp-backend/src/main/clj/solita/etp/schema/valvonta_kaytto.clj
+++ etp-backend/src/main/clj/solita/etp/schema/valvonta_kaytto.clj
@@ -3,15 +3,13 @@
             [solita.etp.schema.common :as common-schema]
             [solita.etp.schema.geo :as geo-schema]))
 
-;; TODO which fields are mandatory?
 (def ValvontaSave
   {:rakennustunnus (schema/maybe common-schema/Rakennustunnus)
-   :katuosoite (schema/maybe common-schema/String100)
+   :katuosoite common-schema/String200
    :postinumero (schema/maybe geo-schema/PostinumeroFI)
-   :ilmoituspaikka (schema/maybe common-schema/Key)
-   :ilmoituspaikka-muu (schema/maybe common-schema/String100)
+   :ilmoituspaikka-id (schema/maybe common-schema/Key)
    :ilmoitustunnus (schema/maybe common-schema/String100)
-   :ilmoitus-katsomispaiva (schema/maybe common-schema/Date)
+   :havaintopaiva (schema/maybe common-schema/Date)
    :valvoja-id (schema/maybe common-schema/Key)})
 
 (def Valvonta (assoc ValvontaSave :id common-schema/Key))
