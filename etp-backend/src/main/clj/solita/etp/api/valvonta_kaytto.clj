30768c6f999ebf5ea1f754d24c943ce76fa43c09
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2021-06-07 13:18:49 +0300

    AE-1340 implement CRUD for käytönvalvonta mock api

--- etp-backend/src/main/clj/solita/etp/api/valvonta_kaytto.clj
+++ etp-backend/src/main/clj/solita/etp/api/valvonta_kaytto.clj
@@ -17,8 +17,16 @@
                     :parameters {:query valvonta-schema/ValvontaQuery}
                     :responses  {200 {:body [valvonta-kaytto-schema/ValvontaStatus]}}
                     :access     rooli-service/paakayttaja?
-                    :handler    (fn [{{:keys [query]} :parameters :keys [db whoami]}]
-                                  (r/response (valvonta-service/find-valvonnat db query)))}}]
+                    :handler    (fn [{{:keys [query]} :parameters :keys [db]}]
+                                  (r/response (valvonta-service/find-valvonnat db query)))}
+      :post        {:summary    "Luo uusi käytönvalvonta"
+                    :access     rooli-service/paakayttaja?
+                    :parameters {:body valvonta-kaytto-schema/ValvontaSave}
+                    :responses  {200 {:body common-schema/Id}}
+                    :handler    (fn [{{:keys [body]} :parameters :keys [db uri]}]
+                                  (api-response/created
+                                   uri
+                                   {:id (valvonta-service/add-valvonta! db body)}))}}]
     ["/:id"
      [""
       {:conflicting true
@@ -26,25 +34,25 @@
                      :parameters {:path {:id common-schema/Key}}
                      :responses  {200 {:body valvonta-kaytto-schema/Valvonta}}
                      :access     rooli-service/paakayttaja?
-                     :handler    (fn [{{{:keys [id]} :path} :parameters :keys [db whoami]}]
+                     :handler    (fn [{{{:keys [id]} :path} :parameters :keys [db]}]
                                    (api-response/get-response
                                      (valvonta-service/find-valvonta db id)
                                      (str "Käytönvalvonta " id " does not exists.")))}
-       :post        {:summary    "Luo uusi käytönvalvonta"
-                     :access     rooli-service/paakayttaja?
-                     :parameters {:body valvonta-kaytto-schema/ValvontaSave}
-                     :responses  {200 {:body common-schema/Id}}
-                     :handler    (fn [{{:keys [body]} :parameters :keys [db whoami uri]}]
-                                   (api-response/created
-                                    uri
-                                    {:id (valvonta-service/add-valvonta! db body)}))}
        :put         {:summary    "Muuta käytönvalvonnan yleisiä tietoja."
                      :access     rooli-service/paakayttaja?
                      :parameters {:path {:id common-schema/Key}
                                   :body valvonta-kaytto-schema/ValvontaSave}
                      :responses  {200 {:body nil}}
                      :handler    (fn [{{{:keys [id]} :path :keys [body]}
-                                      :parameters :keys [db whoami]}]
+                                      :parameters :keys [db]}]
                                    (api-response/ok|not-found
                                      (valvonta-service/update-valvonta! db id body)
-                                     (str "Käytönvalvonta " id " does not exists.")))}}]]]])
+                                     (str "Käytönvalvonta " id " does not exists.")))}
+       :delete    {:summary    "Poista käytönvalvonta"
+                   :access     rooli-service/paakayttaja?
+                   :parameters {:path {:id common-schema/Key}}
+                   :responses  {200 {:body nil}}
+                   :handler    (fn [{{{:keys [id]} :path} :parameters :keys [db]}]
+                                 (api-response/ok|not-found
+                                  (valvonta-service/delete-valvonta! db id)
+                                  (str "Käytönvalvonta " id " does not exists.")))}}]]]])
