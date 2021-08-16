f19f66cf5550102cabc7b02757aea64191786e15
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-08-16 13:04:53 +0300

    AE-1123 - add tests for basic use cases

---
+++ etp-backend/src/test/clj/solita/etp/service/energiatodistus_laskutus_test.clj
@@ -0,0 +1,59 @@
+(ns solita.etp.service.energiatodistus-laskutus-test
+  (:require [clojure.test :as t]
+            [solita.etp.service.energiatodistus-test :as energiatodistus-test]
+            [solita.etp.service.energiatodistus :as service]
+            [solita.etp.test-data.laatija :as laatija-test-data]
+            [solita.etp.test-data.energiatodistus :as energiatodistus-test-data]
+            [solita.etp.test-data.yritys :as yritys-test-data]
+            [solita.etp.test-system :as ts]))
+
+(t/use-fixtures :each ts/fixture)
+
+(t/deftest define-yritys-laskutus
+  (let [laatija-id (-> (laatija-test-data/generate-and-insert! 1) keys first)
+        yritys-id (-> (yritys-test-data/generate-and-insert! 1 laatija-id) keys first)
+        energiatodistukset
+        (map #(assoc % :laskutusosoite-id yritys-id)
+             [(energiatodistus-test-data/generate-add 2013 true)
+              (energiatodistus-test-data/generate-add 2018 true)
+              (energiatodistus-test-data/generate-add 2013 false)
+              (energiatodistus-test-data/generate-add 2018 false)])]
+
+    (doseq [[energiatodistus id]
+            (map vector energiatodistukset
+                 (energiatodistus-test-data/insert! energiatodistukset laatija-id))]
+      (t/is (energiatodistus-test/add-eq-found?
+              (assoc energiatodistus :laskutettava-yritys-id yritys-id)
+              (service/find-energiatodistus ts/*db* id))))))
+
+(t/deftest define-laatija-laskutus
+  (let [laatija-id (-> (laatija-test-data/generate-and-insert! 1) keys first)
+        energiatodistukset
+        (map #(assoc % :laskutusosoite-id -1)
+             [(energiatodistus-test-data/generate-add 2013 true)
+              (energiatodistus-test-data/generate-add 2018 true)
+              (energiatodistus-test-data/generate-add 2013 false)
+              (energiatodistus-test-data/generate-add 2018 false)])]
+
+    (doseq [[energiatodistus id]
+            (map vector energiatodistukset
+                 (energiatodistus-test-data/insert! energiatodistukset laatija-id))]
+      (t/is (energiatodistus-test/add-eq-found?
+              energiatodistus
+              (service/find-energiatodistus ts/*db* id))))))
+
+(t/deftest undefined-laskutusosoite
+  (let [laatija-id (-> (laatija-test-data/generate-and-insert! 1) keys first)
+        energiatodistukset
+        (map #(assoc % :laskutusosoite-id nil :laskutettava-yritys-id nil)
+             [(energiatodistus-test-data/generate-add 2013 true)
+              (energiatodistus-test-data/generate-add 2018 true)
+              (energiatodistus-test-data/generate-add 2013 false)
+              (energiatodistus-test-data/generate-add 2018 false)])]
+
+    (doseq [[energiatodistus id]
+            (map vector energiatodistukset
+                 (energiatodistus-test-data/insert! energiatodistukset laatija-id))]
+      (t/is (energiatodistus-test/add-eq-found?
+              energiatodistus
+              (service/find-energiatodistus ts/*db* id))))))
