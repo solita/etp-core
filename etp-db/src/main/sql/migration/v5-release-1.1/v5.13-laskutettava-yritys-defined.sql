35292388d06b568ca6f96abfe55c2f7fa209302a
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-08-16 14:41:09 +0300

    AE-1123 - update draft and deleted to defined if laskutettava_yritys_id is already defined

--- etp-db/src/main/sql/migration/v5-release-1.1/v5.13-laskutettava-yritys-defined.sql
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.13-laskutettava-yritys-defined.sql
@@ -4,3 +4,7 @@ alter table etp.energiatodistus
 update etp.energiatodistus
   set laskutettava_yritys_defined = true
 where tila_id in (1, 2, 3, 4);
+
+update etp.energiatodistus
+  set laskutettava_yritys_defined = true
+where tila_id in (0, 5) and laskutettava_yritys_id is not null;
