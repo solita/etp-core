c110b6f4a8713256ed7d907b75e6d885e94956c8
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-08-17 23:55:19 +0300

    AE-1123 - ET:n laskutusosoitteen auditin korjaus

--- etp-db/src/main/sql/migration/v5-release-1.1/v5.13-laskutettava-yritys-defined.sql
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.13-laskutettava-yritys-defined.sql
@@ -1,10 +1,6 @@
 alter table etp.energiatodistus
-  add column laskutettava_yritys_defined bool not null default false;
+  add column laskutettava_yritys_defined boolean not null default false;
 
-update etp.energiatodistus
-  set laskutettava_yritys_defined = true
-where tila_id in (1, 2, 3, 4);
-
-update etp.energiatodistus
-  set laskutettava_yritys_defined = true
-where tila_id in (0, 5) and laskutettava_yritys_id is not null;
+alter table audit.energiatodistus
+  add column laskutettava_yritys_defined boolean not null default false;
+call audit.create_audit_procedure('energiatodistus'::name);
