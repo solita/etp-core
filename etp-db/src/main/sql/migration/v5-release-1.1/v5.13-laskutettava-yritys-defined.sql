ca7e9186be3d301a06cebbdc4c0951d45319d6e3
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-08-18 12:23:18 +0300

    AE-1123 - existing data: laskutettava_yritys_defined default true, new data: laskutettava_yritys_defined default false

--- etp-db/src/main/sql/migration/v5-release-1.1/v5.13-laskutettava-yritys-defined.sql
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.13-laskutettava-yritys-defined.sql
@@ -1,6 +1,12 @@
 alter table etp.energiatodistus
-  add column laskutettava_yritys_defined boolean not null default false;
+  add column laskutettava_yritys_defined boolean not null default true;
 
 alter table audit.energiatodistus
-  add column laskutettava_yritys_defined boolean not null default false;
+  add column laskutettava_yritys_defined boolean not null default true;
 call audit.create_audit_procedure('energiatodistus'::name);
+
+alter table etp.energiatodistus
+  alter column laskutettava_yritys_defined set default false;
+
+alter table audit.energiatodistus
+  alter column laskutettava_yritys_defined set default false;
