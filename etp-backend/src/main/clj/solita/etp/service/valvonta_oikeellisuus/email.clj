0e029d35746772364643517c6d86ed659b7d26a6
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-08-21 16:41:33 +0300

    AE-1430 - tietopyyntö-toimenpiteiden sähköpostiviestit

--- etp-backend/src/main/clj/solita/etp/service/valvonta_oikeellisuus/email.clj
+++ etp-backend/src/main/clj/solita/etp/service/valvonta_oikeellisuus/email.clj
@@ -11,13 +11,13 @@
 
 (defn- send-email! [{:keys [to subject body]}]
   (smtp/send-text-email! config/smtp-host
-                    config/smtp-port
-                    config/smtp-username
-                    config/smtp-password
-                    config/email-from-email
-                    config/email-from-name
-                    [to]
-                    subject body "html"))
+                         config/smtp-port
+                         config/smtp-username
+                         config/smtp-password
+                         config/email-from-email
+                         config/email-from-name
+                         [to]
+                         subject body "html"))
 
 (def ^:private signature
   "Tämä on energiatodistuspalvelun lähettämä automaattinen viesti. Älä vastaa tähän viestiin.")
@@ -27,34 +27,53 @@
 
 (defn- paragraph [& body] (str "<p>" (str/join " " body) "</p>"))
 
+(def ^:private rfi-order-description
+  (paragraph
+    "Sinulle on saapunut tietopyyntö koskien energiatodistusta {energiatodistus.id},"
+    "{energiatodistus.perustiedot.katuosoite-fi},"
+    "{energiatodistus.perustiedot.postinumero}"
+    "{energiatodistus.perustiedot.postitoimipaikka-fi}."))
+
+(def ^:private rfi-order-link
+  (link "Katso ja vastaa tietopyyntöön energiatodistuspalvelussa."))
+
 (def ^:private templates
   {;; kevyt valvontamenettely
    :anomaly
-   {:subject "Poikkeamailmoitus koskien energiatodistusta {energiatodistus.id}"
-    :body ""}
+   {:subject
+    "Poikkeamailmoitus koskien energiatodistusta {energiatodistus.id}"
+    :body
+    ""}
 
    ;; tietopyynnön toimenpidetyypit
    :rfi-request
-   {:subject "Tietopyyntö koskien energiatodistusta {energiatodistus.id}"
+   {:subject
+    "Tietopyyntö koskien energiatodistusta {energiatodistus.id}"
     :body
-    (str (paragraph
-           "Sinulle on saapunut tietopyyntö koskien energiatodistusta {energiatodistus.id},"
-           "{energiatodistus.perustiedot.katuosoite-fi},"
-           "{energiatodistus.perustiedot.postinumero}"
-           "{energiatodistus.perustiedot.postitoimipaikka-fi}.")
-         (link "Katso ja vastaa tietopyyntöön energiatodistuspalvelussa.")
-         (paragraph
-          "Tietopyyntöön on vastattava {toimenpide.deadline-date} mennessä."))}
-   :rfi-order {}
-   :rfi-warning {}
-
-   ;; valvonnan toimenpidetyypit
-   :audit-report {:subject "test" :body "test"}
-   :audit-order {:subject "test" :body "test"}
-   :audit-warning {:subject "test" :body "test"}
-
-   ;; lisäselvityspyynnön toimenpidetyypit
-   :rfc-request {:subject "test" :body "test"}})
+    (str
+      rfi-order-description
+      rfi-order-link
+      (paragraph
+        "Tietopyyntöön on vastattava {toimenpide.deadline-date} mennessä."))}
+   :rfi-order
+   {:subject
+    "Kehotus vastata tietopyyntöön koskien energiatodistusta {energiatodistus.id}"
+    :body
+    (str
+      (paragraph
+        "Kehotamme vastaamaan tietopyyntöön {toimenpide.deadline-date} mennessä.")
+      rfi-order-description
+      rfi-order-link)}
+   :rfi-warning
+   {:subject
+    "Varoitus vastata tietopyyntöön koskien energiatodistusta {energiatodistus.id}"
+    :body
+    (str
+      (paragraph
+        "ARA on lähettänyt teille tästä energiatodistuksesta tietopyynnön ja kehotuksen."
+        "ARA antaa varoituksen ja vaatii vastaamaan tietopyyntöön {toimenpide.deadline-date} mennessä.")
+      rfi-order-description
+      rfi-order-link)}})
 
 (defprotocol TemplateValue (view [value]))
 
@@ -70,7 +89,7 @@
 
 (defn interpolate [template values]
   (str/replace template #"\{(.*?)\}"
-    #(view (get-in values (map keyword (-> % second (str/split #"\.")))))))
+               #(view (get-in values (map keyword (-> % second (str/split #"\.")))))))
 
 (defn send-toimenpide-email! [db energiatodistus-id toimenpide]
   (let [energiatodistus (complete-energiatodistus-service/find-complete-energiatodistus
@@ -79,9 +98,9 @@
         toimenpide-type (-> toimenpide :type-id toimenpide/type-key)
         template-values
         {:energiatodistus energiatodistus
-         :laatija laatija
-         :toimenpide toimenpide
-         :host config/service-host}
+         :laatija         laatija
+         :toimenpide      toimenpide
+         :host            config/service-host}
         template (toimenpide-type templates)
         message (map/map-values #(interpolate % template-values) template)]
     (send-email! (-> message
