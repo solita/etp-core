60b0e852f20d91786d105b4916e638b8c02c06b2
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2021-08-27 09:13:16 +0300

    fix

--- etp-db/src/main/sql/migration/repeatable/vk_template/r-0-vk-toimenpidetype.sql
+++ etp-db/src/main/sql/migration/repeatable/vk_template/r-0-vk-toimenpidetype.sql
@@ -1,11 +1,11 @@
 insert into vk_toimenpidetype (id, label_fi, label_sv, ordinal)
 values
-(0,  'Valvonnan aloitus', 'TODO', 1),
-(1,  'Tietopyyntö', 'TODO', 2),
-(2,  'Tietopyyntö / Kehotus', 'TODO', 3),
-(3,  'Tietopyyntö / Varoitus', 'TODO', 4),
-(4, 'Käskypäätös', 'TODO', 5),
-(5, 'Valvonnan lopetus', 'TODO', 6)
+(0,  'Valvonnan aloitus', 'Valvonnan aloitus (sv)', 1),
+(1,  'Tietopyyntö', 'Tietopyyntö (sv)', 2),
+(2,  'Tietopyyntö / Kehotus', 'Tietopyyntö / Kehotus (sv)', 3),
+(3,  'Tietopyyntö / Varoitus', 'Tietopyyntö / Varoitus (sv)', 4),
+(4, 'Käskypäätös', 'Käskypäätös (sv)', 5),
+(5, 'Valvonnan lopetus', 'Valvonnan lopetus (sv)', 6)
 on conflict (id) do update set
   label_fi = excluded.label_fi,
   label_sv = excluded.label_sv,
