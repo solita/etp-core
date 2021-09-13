7ff107163e03d8a7cf6b5515e44f6603c16acb66
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-09-13 15:08:53 +0300

    AE-1473 - valvonta/oikeellisuus - työjonon järjestys: 1. vastaukset publish_time desc, 2. deadline_date asc, 3. publish_time/create_time desc

--- etp-backend/src/main/sql/solita/etp/db/valvonta-oikeellisuus.sql
+++ etp-backend/src/main/sql/solita/etp/db/valvonta-oikeellisuus.sql
@@ -34,7 +34,10 @@ where
   (energiatodistus.valvonta$valvoja_id = :valvoja-id or
     (energiatodistus.valvonta$valvoja_id is not null) = :has-valvoja or
     (:valvoja-id::int is null and :has-valvoja::boolean is null))
-order by coalesce(last_toimenpide.publish_time, last_toimenpide.create_time) desc
+order by
+  case when last_toimenpide.type_id in (4, 8, 12) then last_toimenpide.publish_time end desc nulls last,
+  last_toimenpide$deadline_date asc nulls last,
+  coalesce(last_toimenpide.publish_time, last_toimenpide.create_time) desc
 limit :limit offset :offset;
 
 -- name: select-valvonnat-laatija
