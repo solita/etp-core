d34a2d7549366f555efff217ec8fd37058d7cd19
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2021-11-01 12:21:57 +0200

    AE-1454: Use fixed list of toimenpide columns

--- etp-db/src/main/sql/migration/repeatable/r-vo-02-last-toimenpide.sql
+++ etp-db/src/main/sql/migration/repeatable/r-vo-02-last-toimenpide.sql
@@ -1,7 +1,17 @@
 
 create or replace view etp.vo_last_toimenpide_v1 as
 select distinct on (toimenpide.energiatodistus_id)
-  toimenpide.*,
+  toimenpide.id, 
+  toimenpide.type_id, 
+  toimenpide.author_id,
+  toimenpide.energiatodistus_id,
+  toimenpide.create_time,
+  toimenpide.publish_time,
+  toimenpide.deadline_date,
+  toimenpide.template_id,
+  toimenpide.diaarinumero,
+  toimenpide.description,
+  toimenpide.severity_id,
   etp.vo_toimenpide_ongoing(toimenpide) ongoing,
   etp.vo_toimenpide_visible_laatija(toimenpide) visible_laatija,
   toimenpide.type_id in (3, 5, 6, 7, 9, 10, 11) or
