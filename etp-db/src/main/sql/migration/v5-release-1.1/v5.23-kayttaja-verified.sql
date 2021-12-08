d06671e3bfea7b1ebd94d80b1c9d4ae2442bcf39
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-12-08 12:29:02 +0200

    AE-1615 - remove comments

--- etp-db/src/main/sql/migration/v5-release-1.1/v5.23-kayttaja-verified.sql
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.23-kayttaja-verified.sql
@@ -1,12 +1,2 @@
 
 alter table kayttaja add column verifytime timestamp with time zone;
-
-/*
-alter table audit.kayttaja
-  add column verifytime timestamp with time zone;
-call audit.create_audit_procedure('kayttaja'::name);
-drop trigger kayttaja_update_trigger on kayttaja;
-call audit.create_audit_update_trigger(
-  'kayttaja'::name, 'kayttaja'::name,
-  audit.update_condition('kayttaja'::name));
-*/
