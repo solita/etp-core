203eae074311941564e5915ebe73d9d68fe50aa5
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2021-12-27 14:32:11 +0200

    AE-1606 - test for find-valvonta as laatija

--- etp-backend/src/test/clj/solita/etp/service/valvonta_oikeellisuus_test.clj
+++ etp-backend/src/test/clj/solita/etp/service/valvonta_oikeellisuus_test.clj
@@ -5,7 +5,8 @@
             [solita.etp.test-data.laatija :as laatija-test-data]
             [solita.etp.test-data.energiatodistus :as energiatodistus-test-data]
             [solita.etp.service.valvonta-oikeellisuus :as service]
-            [solita.etp.test-data.kayttaja :as kayttaja-test-data]))
+            [solita.etp.test-data.kayttaja :as kayttaja-test-data]
+            [solita.etp.test :as etp-test]))
 
 (t/use-fixtures :each ts/fixture)
 
@@ -19,3 +20,23 @@
                                         id)]
     (t/is (some? valvonta))
     (t/is (= valvonta {:id id, :pending false, :valvoja-id nil, :ongoing false}))))
+
+(t/deftest find-valvonta-laatija
+  (let [[laatija-id _] (laatija-test-data/generate-and-insert!)
+        [laatija2-id _] (laatija-test-data/generate-and-insert!)
+        [id _] (energiatodistus-test-data/generate-and-insert!
+                 2018 true laatija-id)
+        valvonta (service/find-valvonta (ts/db-user laatija-id)
+                                        (test-whoami/laatija laatija-id)
+                                        id)]
+    (t/is (some? valvonta))
+    (t/is (= valvonta {:id id, :pending false, :valvoja-id nil, :ongoing false}))
+
+    (t/is (= (etp-test/catch-ex-data
+               #((service/find-valvonta (ts/db-user laatija2-id)
+                                        (test-whoami/laatija laatija2-id)
+                                        id)))
+             {:type   :forbidden,
+              :reason (str "User " laatija2-id
+                           " is not allowed to access laatija: " laatija-id
+                           " valvonta information.")}))))
