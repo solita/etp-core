eb128934cc058d5ed30727e9cdc3c8ab9bfabeb9
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2022-02-10 13:24:26 +0200

    AE-1600 - fix tests

--- etp-backend/src/test/clj/solita/etp/test_data/laatija.clj
+++ etp-backend/src/test/clj/solita/etp/test_data/laatija.clj
@@ -6,8 +6,7 @@
             [solita.etp.service.kayttaja-laatija :as kayttaja-laatija-service]))
 
 (defn generate-adds [n]
-  (map #(generators/complete {:partner false
-                              :henkilotunnus %1
+  (map #(generators/complete {:henkilotunnus %1
                               :email %2
                               :patevyystaso (rand-nth [1 2])}
                              laatija-schema/KayttajaLaatijaAdd)
