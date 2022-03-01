34f55e7834d5fd095127be3c32d7e86d439a2fad
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2022-03-01 22:06:48 +0200

    AE-1686 - we only need to check the case where vastaanottajan_tarkenne is null

--- etp-db/src/main/sql/migration/v5-release-1.1/v5.28-yritys-ytunnus-nimi-key.sql
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.28-yritys-ytunnus-nimi-key.sql
@@ -1,2 +1,3 @@
 
-create unique index if not exists yritys_ytunnus_nimi_key on yritys (ytunnus, nimi) where (not deleted);
+create unique index if not exists yritys_ytunnus_nimi_key on etp.yritys (ytunnus, nimi)
+  where (not deleted and vastaanottajan_tarkenne is null);
