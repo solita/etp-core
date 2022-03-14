e89a92c92a6498bb4b144ce7152ed4bc34f22f56
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-03-14 13:20:03 +0200

    AE-1678 Activate audit earlier

--- etp-db/src/main/sql/migration/v5-release-1.1/v5.30-yritys-type.sql
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.30-yritys-type.sql
@@ -1,10 +1,9 @@
 call create_classification('yritystype'::name);
+call audit.activate('yritystype'::name);
 
 -- The record used as default value is immediately inserted.
 -- See the repeatable schema for up-to-date contents.
 insert into yritystype (id, label_fi, label_sv, ordinal)
 values (1, 'Elinkeinoelämä (03)', 'Elinkeinoelämä (03) (sv)', 4);
-
-call audit.activate('yritystype'::name);
 
 alter table yritys add column type_id int not null default 1 references yritystype(id);
