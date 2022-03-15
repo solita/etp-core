c79e35e33dbe971b72e602cf4c0a8e3433a42b0b
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-03-15 15:03:36 +0200

    AE-1678 Translate yritystype entries

--- etp-db/src/main/sql/migration/repeatable/r-yritystype.sql
+++ etp-db/src/main/sql/migration/repeatable/r-yritystype.sql
@@ -1,7 +1,7 @@
 insert into yritystype (id, label_fi, label_sv, ordinal)
 values
-(1, 'Elinkeinoelämä (03)', 'Elinkeinoelämä (03) (sv)', 2),
-(2, 'Paikallishallinto (01)', 'Paikallishallinto (01) (sv)', 3)
+(1, 'Elinkeinoelämä (03)', 'Affärsliv (03)', 2),
+(2, 'Paikallishallinto (01)', 'Lokal förvaltning (01)', 3)
 on conflict (id) do update set
   label_fi = excluded.label_fi,
   label_sv = excluded.label_sv,
