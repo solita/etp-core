bac6840d2f5e8e61fb98c38c0dcee6dad15693c2
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-06-03 15:25:20 +0300

    AE-1728 Rename Tietopyyntö to Tietopyyntö 2021

--- etp-db/src/main/sql/migration/repeatable/vk_template/r-0-vk-toimenpidetype.sql
+++ etp-db/src/main/sql/migration/repeatable/vk_template/r-0-vk-toimenpidetype.sql
@@ -1,7 +1,7 @@
 insert into vk_toimenpidetype (id, label_fi, label_sv, ordinal)
 values
 (0, 'Valvonnan aloitus', 'Valvonnan aloitus (sv)', 1),
-(1, 'Tietopyyntö', 'Begäran om uppgifter', 2),
+(1, 'Tietopyyntö 2021', 'Begäran om uppgifter', 2),
 (2, 'Kehotus', ' Uppmaning', 3),
 (3, 'Varoitus', 'Varning', 4),
 (4, 'Käskypäätös', 'Käskypäätös (sv)', 5),
