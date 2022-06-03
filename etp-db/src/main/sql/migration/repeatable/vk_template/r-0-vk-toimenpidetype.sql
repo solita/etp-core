b60e74696ce893c20a87aa3ec2dace65e3cf8bcc
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-06-03 15:26:16 +0300

    AE-1728 Mark Tietopyyntö 2021 as valid=false
    This alone does not do much, but it is expected that front-end will
    stop offering this option for new toimepiteet.

--- etp-db/src/main/sql/migration/repeatable/vk_template/r-0-vk-toimenpidetype.sql
+++ etp-db/src/main/sql/migration/repeatable/vk_template/r-0-vk-toimenpidetype.sql
@@ -1,12 +1,13 @@
-insert into vk_toimenpidetype (id, label_fi, label_sv, ordinal)
+insert into vk_toimenpidetype (id, label_fi, label_sv, ordinal, valid)
 values
-(0, 'Valvonnan aloitus', 'Valvonnan aloitus (sv)', 1),
-(1, 'Tietopyyntö 2021', 'Begäran om uppgifter', 2),
-(2, 'Kehotus', ' Uppmaning', 3),
-(3, 'Varoitus', 'Varning', 4),
-(4, 'Käskypäätös', 'Käskypäätös (sv)', 5),
-(5, 'Valvonnan lopetus', 'Valvonnan lopetus (sv)', 6)
+(0, 'Valvonnan aloitus', 'Valvonnan aloitus (sv)', 1, true),
+(1, 'Tietopyyntö 2021', 'Begäran om uppgifter 2021', 2, false),
+(2, 'Kehotus', ' Uppmaning', 3, true),
+(3, 'Varoitus', 'Varning', 4, true),
+(4, 'Käskypäätös', 'Käskypäätös (sv)', 5, true),
+(5, 'Valvonnan lopetus', 'Valvonnan lopetus (sv)', 6, true)
 on conflict (id) do update set
   label_fi = excluded.label_fi,
   label_sv = excluded.label_sv,
-  ordinal = excluded.ordinal;
+  ordinal = excluded.ordinal,
+  valid = excluded.valid;
