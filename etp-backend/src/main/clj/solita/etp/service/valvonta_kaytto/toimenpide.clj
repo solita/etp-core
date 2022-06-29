5fd232a018486f786e0934ce15b0d4c8ee87c9c8
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-06-29 12:36:08 +0300

    AE-1724 Diaarinumero in every toimenpide except :case
    Seems like solita.etp.service.valvonta-kaytto.toimenpide/case-closed?
    is no longer used, so that could be cleaned out as well.

--- etp-backend/src/main/clj/solita/etp/service/valvonta_kaytto/toimenpide.clj
+++ etp-backend/src/main/clj/solita/etp/service/valvonta_kaytto/toimenpide.clj
@@ -19,11 +19,9 @@
   (contains? type-key-set (-> toimenpide :type-id type-key)))
 
 (def case-open? (partial type? :case))
-(def case-closed? (partial type? :closed))
 (def send-tiedoksi? (partial type? :rfi-request))
-(def decision-order? (partial type? :decision-order))
 
 (def asha-toimenpide?
   (partial some-type? #{:rfi-request :rfi-order :rfi-warning}))
 
-(def with-diaarinumero? (some-fn asha-toimenpide? case-closed? decision-order?))
+(def with-diaarinumero? (comp not (partial type? :case)))
