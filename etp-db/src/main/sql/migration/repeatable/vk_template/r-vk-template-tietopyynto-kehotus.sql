90fd91843ba0a37fac1938d54ee09b633f8a11f8
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-09-14 12:08:04 +0300

    AE-1786 Fix the tiedoksi section in the new Kehotus

--- etp-db/src/main/sql/migration/repeatable/vk_template/r-vk-template-tietopyynto-kehotus.sql
+++ etp-db/src/main/sql/migration/repeatable/vk_template/r-vk-template-tietopyynto-kehotus.sql
@@ -111,10 +111,20 @@ $$
         <td>Lagen om energicertifikat för byggnader (50/2013)</td>
     </tr>
     <tr>
-        <td><b>Tiedoksi/För kännedom:</b></td>
-        <td>
-            <div>{{#tiedoksi}}{{.}}<br/>{{/tiedoksi}}</div>
-        </td>
+        <td colspan="2"><b>Tiedoksi/För kännedom:</b></td>
+    </tr>
+    <tr>
+      <td colspan="2">
+        <ul class="mt-0">
+          {{#tiedoksi}}
+            <li class="mt-0 slim">
+            {{#rooli}}<span class="list-item">{{.}}</span>{{/rooli}}
+            <span class="list-item nowrap">{{nimi}}</span>
+            {{#email}}<span class="list-item nowrap">{{.}}</span>{{/email}}
+            </li>
+          {{/tiedoksi}}
+        </ul>
+      </td>
     </tr>
 </table>
 $$)
