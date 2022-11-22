377e7e0d94f41d7ea4d7c80c022f57d74f9c55cf
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-11-22 12:34:01 +0200

    AE-1815 Postpone removal of api_key_hash from laatija
    This improves compatibility of the new layout with old code, which
    gets momentarily run with the new layout during deployment of the
    upgrade.
    
    The removal can be done afterwards in a separate changeset.

--- etp-db/src/main/sql/migration/v5-release-1.1/v5.33-kayttaja-api-key-hash.sql
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.33-kayttaja-api-key-hash.sql
@@ -5,10 +5,6 @@ set api_key_hash = laatija.api_key_hash
 from laatija
 where kayttaja.id = laatija.id;
 
-alter table laatija drop column api_key_hash;
-
 alter table audit.kayttaja add column api_key_hash text;
-alter table audit.laatija drop column api_key_hash;
 
 call audit.create_audit_procedure('kayttaja'::name);
-call audit.create_audit_procedure('laatija'::name);
