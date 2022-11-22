ac6df335a608b55e59fbca26af3ee8017fa05976
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-11-22 15:10:09 +0200

    AE-1815 Refresh audit triggers on kayttaja table
    This also remedies a missing trigger on the valvoja column.

--- etp-db/src/main/sql/migration/v5-release-1.1/v5.33-kayttaja-api-key-hash.sql
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.33-kayttaja-api-key-hash.sql
@@ -7,4 +7,7 @@ where kayttaja.id = laatija.id;
 
 alter table audit.kayttaja add column api_key_hash text;
 
+drop trigger if exists kayttaja_update_trigger on kayttaja;
 call audit.create_audit_procedure('kayttaja'::name);
+call audit.create_audit_update_trigger('kayttaja'::name, 'kayttaja'::name,
+  audit.update_condition('kayttaja'::name));
