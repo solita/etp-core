bc7c2d765177c60d0d465c07cd3e28ffdcf66f9a
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-11-29 18:28:50 +0200

    AE-1809 Signed url verification

--- etp-backend/src/test/clj/solita/common/cf_signed_url_test.clj
+++ etp-backend/src/test/clj/solita/common/cf_signed_url_test.clj
@@ -1,80 +1,42 @@
 (ns solita.common.cf-signed-url-test
   (:require [clojure.test :as t]
-            [clojure.string :as str]
             [clojure.java.io :as io]
-            [solita.common.cf-signed-url :as signed-url]
-            [solita.etp.service.json :as json])
-  (:import (java.util Base64)
-           (org.bouncycastle.asn1.x509 SubjectPublicKeyInfo)
-           (org.bouncycastle.crypto.digests SHA1Digest)
-           (org.bouncycastle.crypto.signers RSADigestSigner)
-           (org.bouncycastle.crypto.util PublicKeyFactory)))
-
-(defn pem->public-key [parsed-pem]
-  (cond
-    (instance? SubjectPublicKeyInfo parsed-pem)
-    (-> parsed-pem
-        PublicKeyFactory/createKey)
-    :else
-    (throw (ex-info "unexpected kind of PEM"))))
-
-(defn pem-string->public-key [pem-text]
-  (-> pem-text
-      signed-url/str->parsed-pem
-      pem->public-key))
-
-(defn split-at-char [s c]
-  (let [index-of-c (str/index-of s c)]
-    (if (nil? index-of-c)
-      [s ""]
-      [(.substring s 0 index-of-c)
-       (.substring s (inc index-of-c))])))
-
-(defn querystring-safe-base64->bytes [s]
-  (let [decoder (Base64/getDecoder)
-        base64 (-> s
-                   (.replace \- \+)
-                   (.replace \_ \=)
-                   (.replace \~ \/))]
-    (.decode decoder base64)))
-
-(defn signed-url->components [surl]
-  (let [[base-url params-text] (split-at-char surl \?)
-        params (into {}
-                     (-> params-text
-                         (str/split #"&")
-                         (->> (map #(str/split % #"=")))))]
-    {:base-url base-url
-     :expires (-> params (get "Expires") Integer/parseInt)
-     :signature (-> params
-                    (get "Signature")
-                    querystring-safe-base64->bytes)
-     :key-pair-id (-> params (get "Key-Pair-Id"))}))
-
-(defn assert-valid-signed-url! [signed-url expected-key-pair-id public-key]
-  (let [{:keys [base-url expires key-pair-id signature]} (signed-url->components signed-url)
-        policy-document-bytes (-> (signed-url/policy-document base-url expires)
-                                  json/write-value-as-string
-                                  .getBytes)
-        signer (RSADigestSigner. (SHA1Digest.))]
-    (.init signer false public-key)
-    (.update signer policy-document-bytes 0 (count policy-document-bytes))
-    (assert (.verifySignature signer signature))
-    (assert (= expected-key-pair-id key-pair-id))))
+            [solita.common.cf-signed-url :as signed-url]))
 
 (t/deftest signed-url-roundtrip-test
   (let [private-key (-> "cf-signed-url/example.key.pem" io/resource slurp signed-url/pem-string->private-key)
-        public-key (-> "cf-signed-url/example.pub.pem" io/resource slurp pem-string->public-key)
+        public-key (-> "cf-signed-url/example.pub.pem" io/resource slurp signed-url/pem-string->public-key)
         key-pair-id "L3G17K3Y"
         base-url "https://energiatodistusrekisteri.fi"
-        expires (+ 60 (signed-url/unix-time))
+        time-issued (signed-url/unix-time)
+        expires (+ time-issued 60)
+        time-late-test (+ expires 10)
+        optimistic-expiration (+ time-late-test 10)
         url (signed-url/url->signed-url base-url
                                         expires
                                         {:key-pair-id key-pair-id
                                          :private-key private-key})
-        url-components (signed-url->components url)]
-    ;; This pretty much just tests the deserialization
-    (t/is (= base-url (:base-url url-components)))
-    (t/is (= expires (:expires url-components)))
-    (t/is (= key-pair-id (:key-pair-id url-components)))
-    (assert-valid-signed-url! url key-pair-id public-key)))
+        verify-keys {:key-pair-id key-pair-id
+                     :public-key public-key}]
+
+    ;; Positive verification
+    (t/is (nil? (signed-url/signed-url-problem url verify-keys)))
+
+    ;; Verify after expiration
+    (t/is (= :expired-url
+             (signed-url/signed-url-problem-at url
+                                               time-late-test
+                                               verify-keys)))
+
+    ;; Attempt replacing the expiration stamp
+    (t/is (= :invalid-signature
+             (signed-url/signed-url-problem-at (-> url
+                                                   (.replace (str expires)
+                                                             (str optimistic-expiration)))
+                                               time-late-test
+                                               verify-keys)))
+
+    ;; Tamper by adding extra parameters For now this is a format
+    ;; error, since we do not expect to use query parameters at all
+    (t/is (= :format
+             (signed-url/signed-url-problem (str url "&asd=basd") verify-keys)))))
