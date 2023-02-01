ee69d03c4d29257121d7a1ad6821c409472646d4
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2023-02-01 15:46:50 +0200

    Test script for Legacy API

--- etp-backend/src/test/resources/legacy-api/post.sh
+++ etp-backend/src/test/resources/legacy-api/post.sh
@@ -6,12 +6,12 @@ dev='https://private.kehitys.energiatodistusrekisteri.fi'
 test='https://private.testi.energiatodistusrekisteri.fi'
 
 server=${!1}
-file=${2-'complete-2018.json'}
+file=${2-'esimerkki-2018.xml'}
 version=${3-'2018'}
 user=${4-'laatija@solita.fi:asdfasdf'}
 
 curl -v -d "@$file" \
-  -H "Content-Type: application/json" \
+  -H "Content-Type: application/xml" \
   -H 'User-Agent:' \
   -u $user \
-  "$server/api/external/energiatodistukset/$version"
+  "$server/api/external/energiatodistukset/legacy/$version"
