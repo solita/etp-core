a488541c42f105f07cfa458534014da0f4db7f3f
Juho Leinonen <juho.leinonen@solita.fi>
2023-04-04 10:44:36 +0300

    AE-1850: Remove unique constraint from kayttaja_aineisto table
    * remove unnecessary on conflict clause from insert-kayttaja-aineisto! as the existing rows for the user are already deleted before the insert

--- etp-backend/src/main/sql/solita/etp/db/aineisto.sql
+++ etp-backend/src/main/sql/solita/etp/db/aineisto.sql
@@ -3,15 +3,13 @@ select
   aineisto_id, valid_until, ip_address
 from kayttaja_aineisto
 where kayttaja_id = :kayttaja-id and
+--       Null check pois?
       (:ip-address::inet is null or (:ip-address)::inet <<= ip_address::inet) and
       valid_until > now();
 
 -- name: insert-kayttaja-aineisto!
 insert into kayttaja_aineisto (kayttaja_id, aineisto_id, valid_until, ip_address)
-values (:kayttaja-id, :aineisto-id, :valid-until, ((:ip-address)::inet)::text)
-on conflict (kayttaja_id, aineisto_id) do update set
-  valid_until = excluded.valid_until,
-  ip_address = excluded.ip_address;
+values (:kayttaja-id, :aineisto-id, :valid-until, ((:ip-address)::inet)::text);
 
 -- name: delete-kayttaja-aineisto!
 delete from kayttaja_aineisto
---
+++ etp-db/src/main/sql/migration/v5-release-1.1/v5.35-remove-kayttaja_aineisto-constraint.sql
@@ -0,0 +1,2 @@
+alter table kayttaja_aineisto
+    drop constraint kayttaja_aineisto_kayttaja_id_aineisto_id_key;
