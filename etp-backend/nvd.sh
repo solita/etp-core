ebb84657375fd5dc093b692ad4ede1d43c0ec59e
Juho Leinonen <juho.leinonen@solita.fi>
2023-04-24 11:50:44 +0300

    Fix aliases in nvd script

--- etp-backend/nvd.sh
+++ etp-backend/nvd.sh
@@ -2,4 +2,4 @@
 set -e
 
 clojure -Ttools install nvd-clojure/nvd-clojure '{:mvn/version "3.1.0"}' :as nvd
-clojure -J-Dclojure.main.report=stderr -Tnvd nvd.task/check :classpath \""$(clojure -Spath -A:any:aliases)\"" :config-filename \""nvd-config.edn\""
+clojure -J-Dclojure.main.report=stderr -Tnvd nvd.task/check :classpath \""$(clojure -Spath)\"" :config-filename \""nvd-config.edn\""
