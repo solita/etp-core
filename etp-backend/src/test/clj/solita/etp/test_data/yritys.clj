3c423e52b6428863f314594d119ca3e2e00ac0f4
Juho Leinonen <juho.leinonen@solita.fi>
2023-05-05 15:45:20 +0300

    Generate new y-tunnus sets every time the generate function is called in tests

--- etp-backend/src/test/clj/solita/etp/test_data/yritys.clj
+++ etp-backend/src/test/clj/solita/etp/test_data/yritys.clj
@@ -4,12 +4,19 @@
             [solita.etp.schema.yritys :as yritys-schema]
             [solita.etp.service.yritys :as yritys-service]))
 
+(def used-y-tunnukset (atom #{}))
+
 (defn generate-adds [n]
-  (take n (map #(generators/complete {:ytunnus %
-                                      :verkkolaskuoperaattori (rand-int 32)
-                                      :type-id 1}
-                                     yritys-schema/YritysSave)
-               generators/unique-ytunnukset)))
+  (let [generated-data
+        (take (* n 10000) (map #(generators/complete {:ytunnus                %
+                                            :verkkolaskuoperaattori (rand-int 32)
+                                            :type-id                1}
+                                           yritys-schema/YritysSave)
+                     generators/unique-ytunnukset))
+        return-value (take n (remove #(contains? @used-y-tunnukset (:ytunnus %)) generated-data))
+        added-y-tunnukset (map :ytunnus return-value)]
+    (swap! used-y-tunnukset into added-y-tunnukset)
+    return-value))
 
 (def generate-updates generate-adds)
 
