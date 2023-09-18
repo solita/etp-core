86204d5a4fcf9cfd9a19bb2ab1a293c3cb6a7d71
Juho Leinonen <juho.leinonen@solita.fi>
2023-09-18 15:50:15 +0300

    AE-1960: Add sakkopäätös / kuulemiskirje document template

---
+++ etp-db/src/main/sql/migration/repeatable/vk_template/r-vk-template-sakkopaatos-kuulemiskirje.sql
@@ -0,0 +1,149 @@
+insert into vk_template (id,
+                         label_fi,
+                         label_sv,
+                         ordinal,
+                         toimenpidetype_id,
+                         language,
+                         valid,
+                         tiedoksi,
+                         content)
+values (7, 'Sakkopäätös / kuulemiskirje', 'Sakkopäätös / kuulemiskirje (sv)', 1, 14, 'fi', true, true,
+        $$
+<div class="otsikko">
+    <b>Kuulemiskirje</b>
+    <b>{{päivä}}</b> <br/>
+    Dnro: {{diaarinumero}}
+</div>
+
+<div class="vastaanottaja">
+    {{#omistaja-henkilo}}
+    {{sukunimi}} {{etunimi}}<br/>
+    {{jakeluosoite}}<br/>
+    {{postinumero}} {{postitoimipaikka}}
+    {{/omistaja-henkilo}}
+    {{#omistaja-yritys}}
+    {{nimi}}<br/>
+    {{vastaanottajan-tarkenne}}<br/>
+    {{jakeluosoite}}<br/>
+    {{postinumero}} {{postitoimipaikka}}
+    {{/omistaja-yritys}}
+</div>
+
+<h1>Kuuleminen uhkasakon tuomitsemisesta</h1>
+
+<div>
+    Rakennuksen omistaja: {{#omistaja-henkilo}}{{sukunimi}}
+    {{etunimi}}{{/omistaja-henkilo}}{{#omistaja-yritys}}{{nimi}}{{/omistaja-yritys}}<br/>
+    Rakennus: {{#kohde}}{{katuosoite}} {{postinumero}} {{postitoimipaikka}}{{/kohde}}
+</div>
+
+<h2>Asian tausta</h2>
+
+<!-- TODO: Varmista, että tässä on tosiaan kyse käskypäätös / varsinainen päätös -toimenpiteen päivämäärästä -->
+<p>ARA on {{#aiemmat-toimenpiteet}}{{kaskypaatos-varsinainen-paatos-pvm}}{{/aiemmat-toimenpiteet}} tekemällään päätöksellä velvoittanut
+    {{#omistaja-henkilo}}
+    {{etunimi}} {{sukunimi}}
+    {{/omistaja-henkilo}}
+    {{#omistaja-yritys}}
+    {{nimi}}
+    {{/omistaja-yritys}}
+    hankkimaan osoitteessa {{#kohde}}{{katuosoite}} {{postinumero}} {{postitoimipaikka}}{{/kohde}} sijaitsevalle
+    rakennukselle energiatodistuksen viimeistään {{#aiemmat-toimenpiteet}}{{kaskypaatos-varsinainen-paatos-maarapaiva}}{{/aiemmat-toimenpiteet}} ja esittämään
+    energiatodistuksen ARAlle. Päätöksen tehosteeksi ARA on asettanut
+    {{#tyyppikohtaiset-tiedot}}{{fine}}{{/tyyppikohtaiset-tiedot}} euron suuruisen kiinteän uhkasakon.
+    Päätöksen mukaan uhkasakko tuomitaan maksettavaksi, mikäli ARAn käskyä ei ole annetussa määräajassa noudatettu.</p>
+
+<p>ARAn päätökseen ei ole haettu muutosta, eli päätös on lainvoimainen. Energiatodistusta ei ole hankittu eikä esitetty
+    ARAlle määräajassa. Näin ollen ARA aikoo tehdä päätöksen
+    {{#tyyppikohtaiset-tiedot}}{{fine}}{{/tyyppikohtaiset-tiedot}} euron uhkasakon tuomitsemisesta maksuun.</p>
+
+<h2>Asianosaisen kuuleminen</h2>
+
+<p>Ennen asian ratkaisemista ARA varaa Teille hallintolain (434/2003) 34 §:n mukaisen tilaisuuden lausua mielipiteenne
+    asiasta sekä antaa selitys sellaisista vaatimuksista ja selvityksistä, jotka voivat vaikuttaa asian ratkaisuun.
+    Vastaus on annettava viimeistään {{määräpäivä}}. Vastauksen antamatta jättäminen ei
+    estä asian ratkaisemista.</p>
+
+<p>Vastaus pyydetään toimittamaan ARAn kirjaamoon joko sähköpostitse kirjaamo.ara@ara.fi tai postitse Asumisen
+    rahoitus- ja kehittämiskeskus, PL 30, 15141 Lahti.</p>
+
+{{#valvoja}}
+{{etunimi}} {{sukunimi}}
+{{/valvoja}}<br/>
+energia-asiantuntija
+
+<p class="small-text"><i>Asiakirja on allekirjoitettu sähköisesti asianhallintajärjestelmässä.</i></p>
+
+<div class="sivunvaihto"></div>
+
+<!--RUOTSI-->
+<div class="otsikko">
+    <b>Brev om hörande</b>
+    <b>{{päivä}}</b> <br/>
+    Dnr: {{diaarinumero}}
+</div>
+
+<div class="vastaanottaja">
+    {{#omistaja-henkilo}}
+    {{sukunimi}} {{etunimi}}<br/>
+    {{jakeluosoite}}<br/>
+    {{postinumero}} {{postitoimipaikka}}
+    {{/omistaja-henkilo}}
+    {{#omistaja-yritys}}
+    {{nimi}}<br/>
+    {{vastaanottajan-tarkenne}}<br/>
+    {{jakeluosoite}}<br/>
+    {{postinumero}} {{postitoimipaikka}}
+    {{/omistaja-yritys}}
+</div>
+
+<h1>Hörande om utdömande av vite</h1>
+
+<div>
+    Byggnadens ägare: {{#omistaja-henkilo}}{{sukunimi}}
+    {{etunimi}}{{/omistaja-henkilo}}{{#omistaja-yritys}}{{nimi}}{{/omistaja-yritys}}<br/>
+    Byggnad: {{#kohde}}{{katuosoite}} {{postinumero}} {{postitoimipaikka}}{{/kohde}}
+</div>
+
+<h2>Ärendets bakgrund</h2>
+
+<p>ARA har genom sitt beslut {{#aiemmat-toimenpiteet}}{{kaskypaatos-varsinainen-paatos-pvm}}{{/aiemmat-toimenpiteet}} ålagt
+    {{#omistaja-henkilo}}
+    {{etunimi}} {{sukunimi}}
+    {{/omistaja-henkilo}}
+    {{#omistaja-yritys}}
+    {{nimi}}
+    {{/omistaja-yritys}}
+    att senast {{#aiemmat-toimenpiteet}}{{kaskypaatos-varsinainen-paatos-maarapaiva}}{{/aiemmat-toimenpiteet}} skaffa ett energicertifikat till byggnaden på adressen
+    {{#kohde}}{{katuosoite}} {{postinumero}} {{postitoimipaikka}}{{/kohde}} och visa upp energicertifikatet för ARA. ARA
+    har förenat beslutet med ett fast vite på {{#tyyppikohtaiset-tiedot}}{{fine}}{{/tyyppikohtaiset-tiedot}} euro.
+    Enligt beslutet döms vitet ut om ARAs order inte har iakttagits inom utsatt tid.</p>
+
+<p>Ändring i ARAs beslut har inte sökts, dvs. beslutet har vunnit laga kraft. Energicertifikatet har inte skaffats eller
+    visats upp för ARA inom utsatt tid. Således har ARA för avsikt att fatta beslut om att döma ut vitet på
+    {{#tyyppikohtaiset-tiedot}}{{fine}}{{/tyyppikohtaiset-tiedot}} euro.</p>
+
+<h2>Hörande av parter</h2>
+
+<p>Innan ärendet avgörs ger ARA Er tillfälle enligt 34 § i förvaltningsla-gen (434/2003) att uttala Er åsikt i ärendet
+    samt att ge en förklaring till sådana yrkanden och utredningar som kan inverka på avgöran-det av ärendet. Svaret ska
+    ges senast {{määräpäivä}}. Avsaknaden av svar hindrar inte att ärendet avgörs.</p>
+
+<p>Vi ber Er skicka svaret till ARAs registratorskontor antingen per e-post till kirjaamo.ara@ara.fi eller per post till
+    Finansierings- och ut-vecklingscentralen för boendet, PB 30, 15141 Lahtis.</p>
+
+{{#valvoja}}
+{{etunimi}} {{sukunimi}}
+{{/valvoja}}<br/>
+energiexpert
+
+<p class="small-text"><i>Handlingen har undertecknats elektroniskt i ärendehanteringssystemet.</i></p>
+$$)
+on conflict (id) do update set label_fi          = excluded.label_fi,
+                               label_sv          = excluded.label_sv,
+                               ordinal           = excluded.ordinal,
+                               toimenpidetype_id = excluded.toimenpidetype_id,
+                               language          = excluded.language,
+                               valid             = excluded.valid,
+                               tiedoksi          = excluded.tiedoksi,
+                               content           = excluded.content;
