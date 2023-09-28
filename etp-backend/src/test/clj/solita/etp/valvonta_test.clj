2459d2125822d30a109fb9ed1bfb439b2870b483
Juho Leinonen <juho.leinonen@solita.fi>
2023-09-28 15:28:07 +0300

    AE-1989: Check in tests that the created Käskypäätös / odotetaan valitusajan umpeutuminen toimenpide is returned through the api

--- etp-backend/src/test/clj/solita/etp/valvonta_test.clj
+++ etp-backend/src/test/clj/solita/etp/valvonta_test.clj
@@ -836,7 +836,33 @@
                                    (mock/json-body new-toimenpide)
                                    (test-kayttajat/with-virtu-user)
                                    (mock/header "Accept" "application/json")))]
-      (t/is (= (:status response) 201)))))
+      (t/is (= (:status response) 201))
+
+      (t/testing "Toimenpide is returned through the api"
+        (let [response (ts/handler (-> (mock/request :get (format "/api/private/valvonta/kaytto/%s/toimenpiteet" valvonta-id))
+                                       (test-kayttajat/with-virtu-user)
+                                       (mock/header "Accept" "application/json")))
+              response-body (j/read-value (:body response) j/keyword-keys-object-mapper)]
+          (t/is (= (:status response) 200))
+          (t/is (= (count response-body) 1))
+          (t/is (= (-> response-body
+                       first
+                       (dissoc :publish-time :create-time))
+                   {:author             {:etunimi  "Asian"
+                                         :id       1
+                                         :rooli-id 2
+                                         :sukunimi "Tuntija"}
+                    :deadline-date      "2023-11-04"
+                    :description        "Odotetaan valitusajan päättymistä"
+                    :diaarinumero       nil
+                    :filename           nil
+                    :henkilot           []
+                    :id                 1
+                    :template-id        nil
+                    :type-id            12
+                    :type-specific-data nil
+                    :valvonta-id        1
+                    :yritykset          []})))))))
 
 (t/deftest adding-and-fetching-valvonta
   (let [kayttaja-id (test-kayttajat/insert-virtu-paakayttaja!)
