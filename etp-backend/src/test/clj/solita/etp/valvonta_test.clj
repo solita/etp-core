af8ebe70bf1e93a9901513a7709ea5a1b435e6d6
Antti Möttönen <antti.mottonen@solita.fi>
2023-10-04 14:26:25 +0300

    Add requested test case

--- etp-backend/src/test/clj/solita/etp/valvonta_test.clj
+++ etp-backend/src/test/clj/solita/etp/valvonta_test.clj
@@ -759,7 +759,7 @@
                                       :sukunimi                 "Talonomistaja"
                                       :postinumero              "00100"
                                       :henkilotunnus            "000000-0000"
-                                      :rooli-description        ""
+                                      :rooli-description        "Omistaja"
                                       :etunimi                  "Testi"
                                       :vastaanottajan-tarkenne  nil
                                       :maa                      "FI"})
@@ -785,7 +785,51 @@
                                        (test-kayttajat/with-virtu-user)
                                        (mock/header "Accept" "application/json")))]
           (t/is (true? @html->pdf-called?))
-          (t/is (= (:status response) 201))))))
+          (t/is (= (:status response) 201))))
+      (t/testing "Toimenpide is returned through the api"
+        (let [response (ts/handler (-> (mock/request :get (format "/api/private/valvonta/kaytto/%s/toimenpiteet" valvonta-id))
+                                       (test-kayttajat/with-virtu-user)
+                                       (mock/header "Accept" "application/json")))
+              response-body (j/read-value (:body response) j/keyword-keys-object-mapper)]
+          (t/is (= (:status response) 200))
+          (t/is (= (count response-body) 1))
+          (println response-body)
+          (t/is (= (-> response-body
+                       first
+                       (dissoc :publish-time :create-time))
+                   {:author             {:etunimi  "Asian"
+                                         :id       1
+                                         :rooli-id 2
+                                         :sukunimi "Tuntija"}
+                    :deadline-date      "2023-07-22"
+                    :description        "Kuvaus"
+                    :diaarinumero       nil
+                    :filename           "haastemies-tiedoksianto.pdf"
+                    :henkilot           [{:email                    nil
+                                          :etunimi                  "Testi"
+                                          :henkilotunnus            "000000-0000"
+                                          :id                       1
+                                          :jakeluosoite             "Testikatu 12"
+                                          :maa                      "FI"
+                                          :postinumero              "00100"
+                                          :postitoimipaikka         "Helsinki"
+                                          :puhelin                  nil
+                                          :rooli-description        "Omistaja"
+                                          :rooli-id                 0
+                                          :sukunimi                 "Talonomistaja"
+                                          :toimitustapa-description nil
+                                          :toimitustapa-id          0
+                                          :valvonta-id              1
+                                          :vastaanottajan-tarkenne  nil}]
+                    :id                 1
+                    :template-id        8
+                    :type-id            11
+                    :type-specific-data {:osapuoli-specific-data [{:document         true
+                                                                   :haastemies-email "haaste@mie.het"
+                                                                   :karajaoikeus-id  1
+                                                                   :osapuoli-id      1}]}
+                    :valvonta-id        valvonta-id
+                    :yritykset          []}))))))
 
   (t/testing "Käskypäätös / Tiedoksianto (Haastemies) toimenpide is created successfully for yritys and document is generated with correct information"
     ;; Add the valvonta
@@ -807,7 +851,7 @@
                                      :postitoimipaikka         "Helsinki"
                                      :puhelin                  nil
                                      :postinumero              "00100"
-                                     :rooli-description        ""
+                                     :rooli-description        "Omistaja"
                                      :maa                      "FI"})
 
       ;; Mock the current time to ensure that the document has a fixed date
@@ -831,7 +875,50 @@
                                        (test-kayttajat/with-virtu-user)
                                        (mock/header "Accept" "application/json")))]
           (t/is (true? @html->pdf-called?))
-          (t/is (= (:status response) 201)))))))
+          (t/is (= (:status response) 201))))
+      (t/testing "Toimenpide is returned through the api"
+        (let [response (ts/handler (-> (mock/request :get (format "/api/private/valvonta/kaytto/%s/toimenpiteet" valvonta-id))
+                                       (test-kayttajat/with-virtu-user)
+                                       (mock/header "Accept" "application/json")))
+              response-body (j/read-value (:body response) j/keyword-keys-object-mapper)]
+          (println response-body)
+          (t/is (= (:status response) 200))
+          (t/is (= (count response-body) 1))
+          (t/is (= (-> response-body
+                       first
+                       (dissoc :publish-time :create-time))
+                   {:author             {:etunimi  "Asian"
+                                         :id       1
+                                         :rooli-id 2
+                                         :sukunimi "Tuntija"}
+                    :deadline-date      "2023-07-22"
+                    :description        "Kuvaus"
+                    :diaarinumero       nil
+                    :filename           "haastemies-tiedoksianto.pdf"
+                    :henkilot           []
+                    :id                 2
+                    :template-id        8
+                    :type-id            11
+                    :type-specific-data {:osapuoli-specific-data [{:document         true
+                                                                   :haastemies-email "haaste@mie.het"
+                                                                   :karajaoikeus-id  1
+                                                                   :osapuoli-id      1}]}
+                    :valvonta-id        valvonta-id
+                    :yritykset          [{:toimitustapa-description nil,
+                                          :toimitustapa-id          0,
+                                          :email                    nil,
+                                          :rooli-id                 0,
+                                          :jakeluosoite             "Testikatu 12",
+                                          :valvonta-id              2,
+                                          :postitoimipaikka         "Helsinki",
+                                          :ytunnus                  nil,
+                                          :puhelin                  nil,
+                                          :nimi                     "Yritysomistaja",
+                                          :postinumero              "00100",
+                                          :id                       1,
+                                          :rooli-description        "Omistaja",
+                                          :vastaanottajan-tarkenne  "Lisäselite C/O",
+                                          :maa                      "FI"}]})))))))
 
 (t/deftest sakkopaatos-kuulemiskirje-test
   (test-kayttajat/insert-virtu-paakayttaja!
