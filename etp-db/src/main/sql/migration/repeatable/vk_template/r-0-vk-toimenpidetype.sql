082161038a59abb65acfdfce58bbf0d03c71fe10
Juho Haapakoski <juho.haapakoski@solita.fi>
2023-10-09 13:38:18 +0300

    Change migration: can add comments and is manually deliverable
    Aran työntekijä toimittaa kirjeen manuaalisesti
    Kommentointimahdollisuus kuten valvonnan lopetus toimenpiteessä

--- etp-db/src/main/sql/migration/repeatable/vk_template/r-0-vk-toimenpidetype.sql
+++ etp-db/src/main/sql/migration/repeatable/vk_template/r-0-vk-toimenpidetype.sql
@@ -15,8 +15,8 @@ values
 (12, 'Käskypäätös / valitusajan odotus ja umpeutuminen', 'Käskypäätös / valitusajan odotus ja umpeutuminen (sv)', 13, true, false, true),
 (14, 'Sakkopäätös / kuulemiskirje', 'Sakkopäätös / kuulemiskirje (sv)', 15, true, true, true),
 (15, 'Sakkopäätös / varsinainen päätös', 'Sakkopäätös / varsinainen päätös (sv)', 16, true, false, false),
-(16, 'Sakkopäätös / tiedoksianto (ensimmäinen postitus)', 'Sakkopäätös / tiedoksianto (ensimmäinen postitus) (sv)', 17, true, false, false),
 (17, 'Sakkopäätös / tiedoksianto (toinen postitus)', 'Sakkopäätös / tiedoksianto (toinen postitus) (sv)', 18, true, false, false),
+(16, 'Sakkopäätös / tiedoksianto (ensimmäinen postitus)', 'Sakkopäätös / tiedoksianto (ensimmäinen postitus) (sv)', 17, true, true, true),
 (18, 'Sakkopäätös / tiedoksianto (Haastemies)', 'Sakkopäätös / tiedoksianto (Haastemies) (sv)', 19, true, false, false),
 (19, 'Sakkopäätös / valitusajan odotus ja umpeutuminen', 'Sakkopäätös / valitusajan odotus ja umpeutuminen (sv)', 20, true, false, false),
 (21, 'Sakkoluettelon lähetys menossa', 'Sakkoluettelon lähetys menossa (sv)', 22, true, false, false)
