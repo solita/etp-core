fc4148c18902287899ef5d30794d21760d607a82
Juho Haapakoski <juho.haapakoski@solita.fi>
2023-10-12 15:31:22 +0300

    AE-1963: Add toimenpide penalty-decision-notice-second-mailing
    Mark it as sakkopaatos-toimenpide
    Test that it:
      * is sakkopaatos-toimenpide
      * is NOT asha-toimenpide
      * can be created
      * is returned through the api once created

--- etp-backend/src/test/clj/solita/etp/valvonta_test.clj
+++ etp-backend/src/test/clj/solita/etp/valvonta_test.clj
@@ -10,6 +10,7 @@
     [solita.etp.service.pdf :as pdf]
     [solita.etp.service.suomifi-viestit :as suomifi-viestit]
     [solita.etp.service.valvonta-kaytto :as valvonta-service]
+    [solita.etp.service.valvonta-kaytto.toimenpide :as toimenpide]
     [solita.etp.test-data.generators :as generators]
     [solita.etp.test-data.kayttaja :as test-kayttajat]
     [solita.etp.test-system :as ts])
@@ -1242,3 +1243,51 @@
                     :valvonta-id        1
                     :yritykset          []})))))))
 
+(t/deftest sakkopaatos-toinen-postitus-test
+  (test-kayttajat/insert-virtu-paakayttaja!
+    {:etunimi  "Asian"
+     :sukunimi "Tuntija"
+     :email    "testi@ara.fi"
+     :puhelin  "0504363675457"})
+  (t/testing "Sakkopäätös / tiedoksianto (toinen postitus)"
+    (let [valvonta-id (valvonta-service/add-valvonta! ts/*db*
+                                                      {:katuosoite        "Testitie 5"
+                                                       :postinumero       "90100"
+                                                       :ilmoituspaikka-id 0})
+          sakkopaatos-toinen-postitus-type-id 17
+          new-toimenpide {:type-id       sakkopaatos-toinen-postitus-type-id
+                          :deadline-date (str (LocalDate/of 2023 11 4))
+                          :template-id   nil
+                          :description   "Sakkopäätös postitetaan uudestaan liitteineen (mukana myös sakkolappu) kirjeitse kirjaamon kautta saantitodistuksella, jos ensimmäinen kirje palautuu takaisin."}
+          response (ts/handler (-> (mock/request :post (format "/api/private/valvonta/kaytto/%s/toimenpiteet" valvonta-id))
+                                   (mock/json-body new-toimenpide)
+                                   (test-kayttajat/with-virtu-user)
+                                   (mock/header "Accept" "application/json")))]
+      (t/is (= (:status response) 201))
+      (t/is (toimenpide/sakkopaatos-toimenpide? {:type-id sakkopaatos-toinen-postitus-type-id}))
+      (t/is (not (toimenpide/asha-toimenpide? {:type-id sakkopaatos-toinen-postitus-type-id})))
+      (t/testing "Toimenpide is returned through the api"
+        (let [response (ts/handler (-> (mock/request :get (format "/api/private/valvonta/kaytto/%s/toimenpiteet" valvonta-id))
+                                       (test-kayttajat/with-virtu-user)
+                                       (mock/header "Accept" "application/json")))
+              response-body (j/read-value (:body response) j/keyword-keys-object-mapper)]
+          (t/is (= (:status response) 200))
+          (t/is (= (count response-body) 1))
+          (t/is (= (-> response-body
+                       first
+                       (dissoc :publish-time :create-time))
+                   {:author             {:etunimi  "Asian"
+                                         :id       1
+                                         :rooli-id 2
+                                         :sukunimi "Tuntija"}
+                    :deadline-date      "2023-11-04"
+                    :description        "Sakkopäätös postitetaan uudestaan liitteineen (mukana myös sakkolappu) kirjeitse kirjaamon kautta saantitodistuksella, jos ensimmäinen kirje palautuu takaisin."
+                    :diaarinumero       nil
+                    :filename           nil
+                    :henkilot           []
+                    :id                 1
+                    :template-id        nil
+                    :type-id            sakkopaatos-toinen-postitus-type-id
+                    :type-specific-data nil
+                    :valvonta-id        1
+                    :yritykset          []})))))))
