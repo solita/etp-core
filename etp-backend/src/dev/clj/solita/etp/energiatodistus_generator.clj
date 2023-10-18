85fcd2c533bb82f506ede563b82794ee7d760d90
Juho Haapakoski <juho.haapakoski@solita.fi>
2023-10-18 16:59:42 +0300

    AE-1992: Add logging to performance testing generator function

--- etp-backend/src/dev/clj/solita/etp/energiatodistus_generator.clj
+++ etp-backend/src/dev/clj/solita/etp/energiatodistus_generator.clj
@@ -9,6 +9,7 @@
 
 ;; This is perhaps not 100% accurate representation on how production data
 ;; looks, but it should be close enough for performance testing purposes.
+;; The added energiatodistukset are just set to signed directly.
 (defn add-energiatodistukset! [db laatija-id versio energiatodistukset]
   (let [db-rows (map #(-> % (assoc :versio versio
                                    :laatija-id laatija-id
@@ -24,11 +25,18 @@
       (map vals db-rows)
       db/default-opts)))
 
+;; Will also add a laatija for the added energiatodistukset.
 (defn add-energiatodistukset-for-performance-testing [db n]
-  (when (< n 100)
-    (println "We are generating ETs with batch size of 100, so the number of ETs must be at least 100."))
-  (with-bindings {#'ts/*db* db}
-    (let [[laatija-id _] (-> (laatija-test-data/generate-and-insert! 1) first)
-          energiatodistukset (energiatodistus-test-data/generate-adds n 2018 true)]
-      (doseq [batch (partition 100 energiatodistukset)]
-        (time (add-energiatodistukset! db laatija-id 2018 batch))))))
+  (let [batch-size 100]
+    (when (< n batch-size)
+      (binding [*out* *err*]
+        (println (str "ERROR: We are generating ETs with batch size of " batch-size ", so the number of ETs must be at least " batch-size "."))))
+    (when (not= (rem n batch-size) 0)
+      (binding [*out* *err*]
+        (println (str "WARNING: The amount to generate (" n ") is not divisible by the batch size (" batch-size "). Only full partitions will be used."))))
+    (with-bindings {#'ts/*db* db}
+      (let [[laatija-id _] (-> (laatija-test-data/generate-and-insert! 1) first)
+            energiatodistukset (energiatodistus-test-data/generate-adds n 2018 true)]
+        (doseq [batch (partition batch-size energiatodistukset)]
+          (time (add-energiatodistukset! db laatija-id 2018 batch))
+          (println (str "Added " batch-size " energiatodistus")))))))
