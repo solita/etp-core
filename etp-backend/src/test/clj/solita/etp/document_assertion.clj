47347d37f1b68bfd0f7784c5188e2afb7eefa756
Juho Leinonen <juho.leinonen@solita.fi>
2023-10-24 09:22:09 +0300

    Document solita.etp.document-assertion/html->pdf-with-assertion

--- etp-backend/src/test/clj/solita/etp/document_assertion.clj
+++ etp-backend/src/test/clj/solita/etp/document_assertion.clj
@@ -5,7 +5,11 @@
 
 (def original-html->pdf pdf/html->pdf)
 
-(defn html->pdf-with-assertion [doc-path-to-compare-to html->pdf-called? html-doc output-stream]
+(defn html->pdf-with-assertion
+  "Wraps solita.etp.service.pdf/html->pdf so that the document can be asserted in html format before it's
+  rendered to pdf. Provide first two parameters by using partial and redef the original function with the
+  returned function."
+  [doc-path-to-compare-to html->pdf-called? html-doc output-stream]
   ;; Mocking the pdf rendering function so that the document contents can be asserted
   ;; Compare the created document to the snapshot
   (t/is (= html-doc
