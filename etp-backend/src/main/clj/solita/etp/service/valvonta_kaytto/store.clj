a61f01addf24576ca20fe37501a172f1d42fcb77
Juho Leinonen <juho.leinonen@solita.fi>
2023-11-09 15:09:52 +0200

    AE-1977: Add docstrings to functions for storing and retrieving käytönvalvonta toimenpide documents

--- etp-backend/src/main/clj/solita/etp/service/valvonta_kaytto/store.clj
+++ etp-backend/src/main/clj/solita/etp/service/valvonta_kaytto/store.clj
@@ -3,22 +3,25 @@
             [solita.etp.service.valvonta-kaytto.osapuoli :as osapuoli]
             [clojure.java.io :as io]))
 
-(def file-key-prefix "valvonta/kaytto")
+(def document-file-key-prefix "valvonta/kaytto")
 
 (defn- file-path [file-key-prefix valvonta-id toimenpide-id osapuoli]
   (cond
     (osapuoli/henkilo? osapuoli) (str file-key-prefix "/" valvonta-id "/" toimenpide-id "/henkilo/" (:id osapuoli))
     (osapuoli/yritys? osapuoli) (str file-key-prefix "/" valvonta-id "/" toimenpide-id "/yritys/" (:id osapuoli))))
 
-(defn store-document [aws-s3-client valvonta-id toimenpide-id osapuoli document]
+(defn store-document
+  "Store the main document of the käytönvalvonta toimenpide to the object storage"
+  [aws-s3-client valvonta-id toimenpide-id osapuoli document]
   (file-service/upsert-file-from-bytes
     aws-s3-client
-    (file-path file-key-prefix valvonta-id toimenpide-id osapuoli)
+    (file-path document-file-key-prefix valvonta-id toimenpide-id osapuoli)
     document))
 
-(defn find-document [aws-s3-client valvonta-id toimenpide-id osapuoli]
-  (file-service/find-file aws-s3-client (file-path file-key-prefix valvonta-id toimenpide-id osapuoli)))
+(defn find-document
+  "Retrieve the main document of the käytönvalvonta toimenpide from the object storage"
+  [aws-s3-client valvonta-id toimenpide-id osapuoli]
+  (file-service/find-file aws-s3-client (file-path document-file-key-prefix valvonta-id toimenpide-id osapuoli)))
 
 (defn info-letter []
   (-> "pdf/Saate_rakennuksen_omistaja_su_ru.pdf" io/resource io/input-stream))
-
