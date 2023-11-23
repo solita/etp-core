5db71357b0a4c9992143667e9da45e17a779b340
Juho Leinonen <juho.leinonen@solita.fi>
2023-11-23 10:15:00 +0200

    AE-1961: AE-1961: Prevent signatures jumping to their own page in sakkopäätös / kuulemiskirje document

--- etp-db/src/main/sql/migration/repeatable/vk_template/r-vk-template-sakkopaatos-kuulemiskirje.sql
+++ etp-db/src/main/sql/migration/repeatable/vk_template/r-vk-template-sakkopaatos-kuulemiskirje.sql
@@ -65,6 +65,7 @@ values (7, 'Sakkopäätös / kuulemiskirje', 'Sakkopäätös / kuulemiskirje (sv
     Vastaus on annettava viimeistään {{määräpäivä}}. Vastauksen antamatta jättäminen ei
     estä asian ratkaisemista.</p>
 
+<div class="page-break-avoid">
 <p>Vastaus pyydetään toimittamaan ARAn kirjaamoon joko sähköpostitse <br/>kirjaamo.ara@ara.fi tai postitse Asumisen
     rahoitus- ja kehittämiskeskus, PL 30, 15141 Lahti.</p>
 
@@ -72,6 +73,7 @@ values (7, 'Sakkopäätös / kuulemiskirje', 'Sakkopäätös / kuulemiskirje (sv
 {{etunimi}} {{sukunimi}}
 {{/valvoja}}<br/>
 energia-asiantuntija
+</div>
 
 <div class="sivunvaihto"></div>
 
@@ -129,8 +131,7 @@ energia-asiantuntija
     samt att ge en förklaring till sådana yrkanden och utredningar som kan inverka på avgörandet av ärendet. Svaret ska
     ges senast {{määräpäivä}}. Avsaknaden av svar hindrar inte att ärendet avgörs.</p>
 
-<div class="sivunvaihto"></div>
-
+<div class="page-break-avoid">
 <p>Vi ber Er skicka svaret till ARAs registratorskontor antingen per e-post till <br/>kirjaamo.ara@ara.fi eller per post till
     Finansierings- och ut-vecklingscentralen för boendet, PB 30, 15141 Lahtis.</p>
 
@@ -138,7 +139,7 @@ energia-asiantuntija
 {{etunimi}} {{sukunimi}}
 {{/valvoja}}<br/>
 energiexpert
-
+</div>
 $$)
 on conflict (id) do update set label_fi          = excluded.label_fi,
                                label_sv          = excluded.label_sv,
