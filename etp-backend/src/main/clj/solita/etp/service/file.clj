90db70f04c19f0b6c5d49902842c197f5dd38342
Juho Haapakoski <juho.haapakoski@solita.fi>
2023-12-05 16:43:08 +0200

    AE-2036: Add function to upsert file in parts

--- etp-backend/src/main/clj/solita/etp/service/file.clj
+++ etp-backend/src/main/clj/solita/etp/service/file.clj
@@ -29,3 +29,52 @@
         (if (= type :resource-not-found)
           false
           (throw e))))))
+
+(defn upsert-file-in-parts
+  "Uploads a file to S3 in parts.
+
+  Given `aws-s3-client`, `key` and a function `upload-parts-fn` uploads
+  the file constructed by parts created by `upload-parts-fn` to S3.
+
+  Does not abort failed multipart uploads. The cleaning of failed multipart
+  uploads depends on the S3 bucket's policy.
+
+  Parameters:
+  - aws-s3-client: The AWS S3 Client.
+  - key (string): The location inside the bucket.
+  - upload-parts-fn (fn [(fn [byte-array -> nil]) -> nil]): A function that
+      is provided the function to upload a single part. All the uploaded parts
+      (except the last one) should be >=5MB.
+
+  Returns:
+  - nil
+
+  Example:
+  (upsert-file-in-parts
+    (user/aws-s3-client)
+    \"foo/bar/baz.txt\"
+    (fn [upload-part]
+           (upload-part (byte-array (repeatedly (* 5 1024 1024) #(rand-int 256))))
+           (upload-part (byte-array (repeatedly (* 2 1024 1024) #(rand-int 256))))))"
+  [aws-s3-client key upload-parts-fn]
+  (let [{:keys [UploadId]} (aws/create-multipart-upload aws-s3-client key)
+        uploaded-parts-vec (atom [])
+        get-next-part-number (let [current-part-number (atom 0)]
+                               ;The first returned value is 1
+                               (fn [] (swap! current-part-number inc)
+                                 @current-part-number))
+        upload-part-fn (fn [content-byte-array]
+                         (let [part-number (get-next-part-number)
+                               {:keys [ETag]} (aws/upload-part aws-s3-client
+                                                               {:key         key
+                                                                :part-number part-number
+                                                                :upload-id   UploadId
+                                                                :body        content-byte-array})]
+                           (swap! uploaded-parts-vec conj [(str part-number) ETag])))]
+    (upload-parts-fn upload-part-fn)
+    (let [uploaded-parts (->> @uploaded-parts-vec
+                              (reduce (fn [result [part-number etag]] (conj result {:ETag       etag
+                                                                                    :PartNumber part-number})) []))]
+      (aws/complete-multipart-upload aws-s3-client {:key            key
+                                                    :upload-id      UploadId
+                                                    :uploaded-parts uploaded-parts}))))
